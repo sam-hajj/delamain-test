da5eef27877fa94ecb6487687c17eb2131830512
sam-hajj <sam@aceaicompany.com>
2020-01-03 13:53:03 +0000

    docs: Outline contribution guidelines

--- src/core/main.py
+++ src/core/main.py
@@ -1,2 +1,3 @@
 2020-01-02T13:22:08 - INITIAL_LOW - docs: Basic README for core
 2020-01-02T13:27:44 - INITIAL_LOW - chore: Add .gitignore
+2020-01-03T13:53:03 - INITIAL_LOW - docs: Outline contribution guidelines
