affdddc1d636f44c172394215bbed6106fd9085a
sam-hajj <sam@aceaicompany.com>
2020-01-04 13:05:20 +0000

    refactor: Initial project structure for core

--- src/core/main.py
+++ src/core/main.py
@@ -1,3 +1,4 @@
 2020-01-02T13:22:08 - INITIAL_LOW - docs: Basic README for core
 2020-01-02T13:27:44 - INITIAL_LOW - chore: Add .gitignore
 2020-01-03T13:53:03 - INITIAL_LOW - docs: Outline contribution guidelines
+2020-01-04T13:05:20 - INITIAL_LOW - refactor: Initial project structure for core
