6c16ea929052d0b94397c8f7a68da885159617e3
sam-hajj <sam@aceaicompany.com>
2020-01-07 11:25:26 +0000

    Initial setup: main.py

--- src/core/main.py
+++ src/core/main.py
@@ -2,3 +2,4 @@
 2020-01-02T13:27:44 - INITIAL_LOW - chore: Add .gitignore
 2020-01-03T13:53:03 - INITIAL_LOW - docs: Outline contribution guidelines
 2020-01-04T13:05:20 - INITIAL_LOW - refactor: Initial project structure for core
+2020-01-07T11:25:26 - INITIAL_LOW - Initial setup: main.py
