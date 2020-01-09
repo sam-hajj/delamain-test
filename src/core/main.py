1d064be266c3592e8da94abc44c437cdbd81dad7
sam-hajj <sam@aceaicompany.com>
2020-01-09 10:42:33 +0000

    Initial setup: main.py

--- src/core/main.py
+++ src/core/main.py
@@ -3,3 +3,4 @@
 2020-01-03T13:53:03 - INITIAL_LOW - docs: Outline contribution guidelines
 2020-01-04T13:05:20 - INITIAL_LOW - refactor: Initial project structure for core
 2020-01-07T11:25:26 - INITIAL_LOW - Initial setup: main.py
+2020-01-09T10:42:33 - INITIAL_LOW - Initial setup: main.py
