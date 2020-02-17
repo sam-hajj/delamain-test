7cd36068cd2ca89e46ca4dea2ea1eef5f32ec460
sam-hajj <sam@aceaicompany.com>
2020-02-17 13:24:58 +0000

    chore: Update requirements.txt

--- src/core/models.py
+++ src/core/models.py
@@ -1,2 +1,3 @@
 2020-02-17T12:11:31 - RAMPING_UP - refactor: Improve reporting structure in core
 2020-02-17T12:37:57 - RAMPING_UP - feat: Implement basic caching in models.py
+2020-02-17T13:24:58 - RAMPING_UP - chore: Update requirements.txt
