3077f847628a854900a6094f754f6da0518de6e9
sam-hajj <sam@aceaicompany.com>
2020-02-20 11:11:10 +0000

    refactor: Improve product structure in unit

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -1,2 +1,3 @@
 2020-02-17T12:40:15 - RAMPING_UP - chore: Update requirements.txt
 2020-02-17T12:56:41 - RAMPING_UP - test: Add initial unit tests for test_core.py
+2020-02-20T11:11:10 - RAMPING_UP - refactor: Improve product structure in unit
