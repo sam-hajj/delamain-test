cee752bc4195cf319f7b62fbe211f520e3ec907f
sam-hajj <sam@aceaicompany.com>
2020-02-20 12:28:25 +0000

    docs: Detail setup instructions

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -1,3 +1,4 @@
 2020-02-17T12:40:15 - RAMPING_UP - chore: Update requirements.txt
 2020-02-17T12:56:41 - RAMPING_UP - test: Add initial unit tests for test_core.py
 2020-02-20T11:11:10 - RAMPING_UP - refactor: Improve product structure in unit
+2020-02-20T12:28:25 - RAMPING_UP - docs: Detail setup instructions
