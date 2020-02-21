7e45ba08111eb488ea94733a290ee59c23d899b0
sam-hajj <sam@aceaicompany.com>
2020-02-21 12:35:41 +0000

    refactor: Improve order structure in core

--- src/core/models.py
+++ src/core/models.py
@@ -2,3 +2,4 @@
 2020-02-17T12:37:57 - RAMPING_UP - feat: Implement basic caching in models.py
 2020-02-17T13:24:58 - RAMPING_UP - chore: Update requirements.txt
 2020-02-21T11:51:36 - RAMPING_UP - feat: Implement basic data processing in models.py
+2020-02-21T12:35:41 - RAMPING_UP - refactor: Improve order structure in core
