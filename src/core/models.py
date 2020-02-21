a53b30d0d579fddc204954826f14d56bbaadad17
sam-hajj <sam@aceaicompany.com>
2020-02-21 11:51:36 +0000

    feat: Implement basic data processing in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -1,3 +1,4 @@
 2020-02-17T12:11:31 - RAMPING_UP - refactor: Improve reporting structure in core
 2020-02-17T12:37:57 - RAMPING_UP - feat: Implement basic caching in models.py
 2020-02-17T13:24:58 - RAMPING_UP - chore: Update requirements.txt
+2020-02-21T11:51:36 - RAMPING_UP - feat: Implement basic data processing in models.py
