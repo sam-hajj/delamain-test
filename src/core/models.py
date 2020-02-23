f4570aa6861cb27834134c129704985317c3545e
sam-hajj <sam@aceaicompany.com>
2020-02-23 14:05:45 +0000

    feat: Implement basic notifications in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -3,3 +3,4 @@
 2020-02-17T13:24:58 - RAMPING_UP - chore: Update requirements.txt
 2020-02-21T11:51:36 - RAMPING_UP - feat: Implement basic data processing in models.py
 2020-02-21T12:35:41 - RAMPING_UP - refactor: Improve order structure in core
+2020-02-23T14:05:45 - RAMPING_UP - feat: Implement basic notifications in models.py
