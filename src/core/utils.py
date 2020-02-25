62b460a0bc3f3037161127c266703eb2b7308d57
sam-hajj <sam@aceaicompany.com>
2020-02-25 15:37:23 +0000

    feat: Implement basic login in utils.py

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,2 +1,3 @@
 2020-02-21T10:52:55 - RAMPING_UP - feat: Add core logic for login service in utils.py
 2020-02-23T12:51:05 - RAMPING_UP - chore: Update requirements.txt
+2020-02-25T15:37:23 - RAMPING_UP - feat: Implement basic login in utils.py
