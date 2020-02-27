8c2f9abadf6f36cf5a3abf7b1d8ffd1991fbb67d
sam-hajj <sam@aceaicompany.com>
2020-02-27 13:31:20 +0000

    chore: Update requirements.txt

--- src/api/server.py
+++ src/api/server.py
@@ -1,2 +1,3 @@
 2020-02-24T12:07:40 - RAMPING_UP - docs: Detail setup instructions
 2020-02-26T12:39:33 - RAMPING_UP - feat: Add core logic for data processing service in server.py
+2020-02-27T13:31:20 - RAMPING_UP - chore: Update requirements.txt
