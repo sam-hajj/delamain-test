b641b6a0f2d9cec454bb012ddff2a9e3c1875771
sam-hajj <sam@aceaicompany.com>
2020-03-03 13:08:24 +0000

    feat: Implement basic data processing in utils.py

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,3 +1,4 @@
 2020-02-21T10:52:55 - RAMPING_UP - feat: Add core logic for login service in utils.py
 2020-02-23T12:51:05 - RAMPING_UP - chore: Update requirements.txt
 2020-02-25T15:37:23 - RAMPING_UP - feat: Implement basic login in utils.py
+2020-03-03T13:08:24 - RAMPING_UP - feat: Implement basic data processing in utils.py
