d7b22560853f105addf4d3484eadefa871582fa6
sam-hajj <sam@aceaicompany.com>
2020-03-03 13:28:25 +0000

    feat: Implement basic notifications in utils.py

--- src/core/utils.py
+++ src/core/utils.py
@@ -2,3 +2,4 @@
 2020-02-23T12:51:05 - RAMPING_UP - chore: Update requirements.txt
 2020-02-25T15:37:23 - RAMPING_UP - feat: Implement basic login in utils.py
 2020-03-03T13:08:24 - RAMPING_UP - feat: Implement basic data processing in utils.py
+2020-03-03T13:28:25 - RAMPING_UP - feat: Implement basic notifications in utils.py
