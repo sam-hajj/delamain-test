5835e3c98dd6584026f254c89ed0ba1fc0905805
sam-hajj <sam@aceaicompany.com>
2020-03-04 11:17:56 +0000

    chore: Update requirements.txt

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -4,3 +4,4 @@
 2020-02-20T12:28:25 - RAMPING_UP - docs: Detail setup instructions
 2020-02-21T12:40:57 - RAMPING_UP - refactor: Improve product structure in unit
 2020-02-29T14:14:10 - RAMPING_UP - refactor: Improve product structure in unit
+2020-03-04T11:17:56 - RAMPING_UP - chore: Update requirements.txt
