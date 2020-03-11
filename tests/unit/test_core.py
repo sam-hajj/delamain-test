6d68a91665de5d78f2a80d9f574b9650e61bec58
sam-hajj <sam@aceaicompany.com>
2020-03-11 12:28:56 +0000

    feat: Add core logic for profile service in test_core.py

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -6,3 +6,4 @@
 2020-02-29T14:14:10 - RAMPING_UP - refactor: Improve product structure in unit
 2020-03-04T11:17:56 - RAMPING_UP - chore: Update requirements.txt
 2020-03-10T12:58:09 - RAMPING_UP - chore: Update requirements.txt
+2020-03-11T12:28:56 - RAMPING_UP - feat: Add core logic for profile service in test_core.py
