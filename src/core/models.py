d8d4913485bd6e74ba54f5890037397b2a4a32e4
sam-hajj <sam@aceaicompany.com>
2020-03-17 13:51:47 +0000

    feat: Add core logic for payments service in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -6,3 +6,4 @@
 2020-02-23T14:05:45 - RAMPING_UP - feat: Implement basic notifications in models.py
 2020-02-24T12:49:01 - RAMPING_UP - feat: Implement basic search in models.py
 2020-03-05T10:01:31 - RAMPING_UP - chore: Update requirements.txt
+2020-03-17T13:51:47 - RAMPING_UP - feat: Add core logic for payments service in models.py
