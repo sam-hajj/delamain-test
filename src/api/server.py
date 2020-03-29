f78c5d1c0919369468a2f8aa2eb350645518424a
sam-hajj <sam@aceaicompany.com>
2020-03-29 12:57:38 +0000

    refactor: Improve inventory structure in api

--- src/api/server.py
+++ src/api/server.py
@@ -18,3 +18,4 @@
 2020-03-24T11:08:33 - RAMPING_UP - chore: Update requirements.txt
 2020-03-26T12:50:06 - RAMPING_UP - docs: Detail setup instructions
 2020-03-29T12:50:18 - RAMPING_UP - test: Add initial unit tests for server.py
+2020-03-29T12:57:38 - RAMPING_UP - refactor: Improve inventory structure in api
