c8e2c433f2ca6d4e636670a088c5aa47731b9a23
sam-hajj <sam@aceaicompany.com>
2020-03-29 12:38:21 +0000

    feat: Add core logic for payments service in test_core.py

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -14,3 +14,4 @@
 2020-03-13T14:31:44 - RAMPING_UP - refactor: Improve product structure in unit
 2020-03-22T11:29:18 - RAMPING_UP - feat: Implement basic profile in test_core.py
 2020-03-26T14:09:32 - RAMPING_UP - feat: Add core logic for payments service in test_core.py
+2020-03-29T12:38:21 - RAMPING_UP - feat: Add core logic for payments service in test_core.py
