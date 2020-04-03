f3f8e94e1622304522054661d01b15223438ff6e
sam-hajj <sam@aceaicompany.com>
2020-04-03 12:47:42 +0000

    chore: Update requirements.txt

--- src/core/utils.py
+++ src/core/utils.py
@@ -16,3 +16,4 @@
 2020-04-01T11:26:33 - RAMPING_UP - chore: Update requirements.txt
 2020-04-01T12:07:28 - RAMPING_UP - test: Add initial unit tests for utils.py
 2020-04-02T12:08:54 - RAMPING_UP - docs: Detail setup instructions
+2020-04-03T12:47:42 - RAMPING_UP - chore: Update requirements.txt
