6bca34d4decdc48513de753f409ed9b2825713f1
sam-hajj <sam@aceaicompany.com>
2020-04-03 14:59:18 +0000

    chore: Update requirements.txt

--- src/core/models.py
+++ src/core/models.py
@@ -22,3 +22,4 @@
 2020-04-03T11:32:35 - RAMPING_UP - docs: Detail setup instructions
 2020-04-03T12:40:19 - RAMPING_UP - feat: Implement basic payments in models.py
 2020-04-03T13:05:03 - RAMPING_UP - feat: Add core logic for search service in models.py
+2020-04-03T14:59:18 - RAMPING_UP - chore: Update requirements.txt
