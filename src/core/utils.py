0ceeb9f139fe85100f4e8f3b7ec2334942b21e75
sam-hajj <sam@aceaicompany.com>
2020-04-05 10:05:50 +0000

    chore: Update requirements.txt

--- src/core/utils.py
+++ src/core/utils.py
@@ -19,3 +19,4 @@
 2020-04-03T12:47:42 - RAMPING_UP - chore: Update requirements.txt
 2020-04-03T13:04:23 - RAMPING_UP - feat: Add core logic for notifications service in utils.py
 2020-04-04T10:55:57 - RAMPING_UP - feat: Implement basic login in utils.py
+2020-04-05T10:05:50 - RAMPING_UP - chore: Update requirements.txt
