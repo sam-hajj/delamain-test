8542da64f9024f8694ffb96a216909ab7725e271
sam-hajj <sam@aceaicompany.com>
2020-04-08 15:58:43 +0000

    chore: Update requirements.txt

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -25,3 +25,4 @@
 2020-04-03T13:42:23 - RAMPING_UP - chore: Update requirements.txt
 2020-04-03T14:34:32 - RAMPING_UP - feat: Add core logic for caching service in test_core.py
 2020-04-07T13:20:35 - RAMPING_UP - feat: Implement basic caching in test_core.py
+2020-04-08T15:58:43 - RAMPING_UP - chore: Update requirements.txt
