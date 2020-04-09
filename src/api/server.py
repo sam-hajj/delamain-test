94a3a4cb9c3ac27ca720046bf1e343d801353227
sam-hajj <sam@aceaicompany.com>
2020-04-09 12:40:21 +0000

    test: Add initial unit tests for server.py

--- src/api/server.py
+++ src/api/server.py
@@ -27,3 +27,4 @@
 2020-04-08T15:23:53 - RAMPING_UP - chore: Update requirements.txt
 2020-04-08T15:54:26 - RAMPING_UP - test: Add initial unit tests for server.py
 2020-04-09T12:27:47 - RAMPING_UP - test: Add initial unit tests for server.py
+2020-04-09T12:40:21 - RAMPING_UP - test: Add initial unit tests for server.py
