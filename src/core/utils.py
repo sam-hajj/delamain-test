97215ae901358af283f3b0eab7c212edf02690b4
sam-hajj <sam@aceaicompany.com>
2020-04-14 10:18:50 +0000

    style: Apply linter fixes across core

--- src/core/utils.py
+++ src/core/utils.py
@@ -26,3 +26,4 @@
 2020-04-08T15:54:31 - RAMPING_UP - refactor: Improve inventory structure in core
 2020-04-12T12:21:45 - COOLING_DOWN - fix: Address minor issues from backlog (related to utils.py)
 2020-04-14T08:26:44 - COOLING_DOWN - test: Refactor tests for reporting for clarity
+2020-04-14T10:18:50 - COOLING_DOWN - style: Apply linter fixes across core
