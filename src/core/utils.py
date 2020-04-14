fdcfaad2127796e3ee3b7a612f11ef0367b04035
sam-hajj <sam@aceaicompany.com>
2020-04-14 10:22:01 +0000

    chore: Update dependencies

--- src/core/utils.py
+++ src/core/utils.py
@@ -27,3 +27,4 @@
 2020-04-12T12:21:45 - COOLING_DOWN - fix: Address minor issues from backlog (related to utils.py)
 2020-04-14T08:26:44 - COOLING_DOWN - test: Refactor tests for reporting for clarity
 2020-04-14T10:18:50 - COOLING_DOWN - style: Apply linter fixes across core
+2020-04-14T10:22:01 - COOLING_DOWN - chore: Update dependencies
