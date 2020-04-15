a121b06b6e5706eef03bdd022f7651497cd64dfb
sam-hajj <sam@aceaicompany.com>
2020-04-15 09:35:56 +0000

    style: Apply linter fixes across core

--- src/core/utils.py
+++ src/core/utils.py
@@ -30,3 +30,4 @@
 2020-04-14T10:22:01 - COOLING_DOWN - chore: Update dependencies
 2020-04-14T10:26:30 - COOLING_DOWN - chore: Update dependencies
 2020-04-15T09:32:07 - COOLING_DOWN - chore: Update dependencies
+2020-04-15T09:35:56 - COOLING_DOWN - style: Apply linter fixes across core
