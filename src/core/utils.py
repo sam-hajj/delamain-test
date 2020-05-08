4ea5e6883cb36c5970767a3f3b2f4d5ecdac6c6c
sam-hajj <sam@aceaicompany.com>
2020-05-08 13:30:54 +0000

    refactor: Code cleanup in utils.py

--- src/core/utils.py
+++ src/core/utils.py
@@ -42,3 +42,4 @@
 2020-05-01T13:06:53 - COOLING_DOWN - fix: Address minor issues from backlog (related to utils.py)
 2020-05-05T14:06:42 - COOLING_DOWN - fix: Address minor issues from backlog (related to utils.py)
 2020-05-05T16:52:37 - COOLING_DOWN - test: Refactor tests for order for clarity
+2020-05-08T13:30:54 - COOLING_DOWN - refactor: Code cleanup in utils.py
