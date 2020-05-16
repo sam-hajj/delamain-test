fd88ae5334295fcf3f45f2c0b81573819dcbfe19
sam-hajj <sam@aceaicompany.com>
2020-05-16 13:53:05 +0000

    fix: Address minor issues from backlog (related to utils.py)

--- src/core/utils.py
+++ src/core/utils.py
@@ -47,3 +47,4 @@
 2020-05-08T14:43:07 - COOLING_DOWN - docs: Add usage examples for caching
 2020-05-13T11:31:19 - COOLING_DOWN - test: Refactor tests for product for clarity
 2020-05-16T13:21:15 - COOLING_DOWN - docs: Add usage examples for search
+2020-05-16T13:53:05 - COOLING_DOWN - fix: Address minor issues from backlog (related to utils.py)
