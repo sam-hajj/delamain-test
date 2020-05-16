1c0bf9a971cb9828f0d1133035e3113e6d1a34f2
sam-hajj <sam@aceaicompany.com>
2020-05-16 12:41:36 +0000

    refactor: Code cleanup in test_utils.py

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -15,3 +15,4 @@
 2020-05-05T14:54:44 - COOLING_DOWN - test: Refactor tests for user for clarity
 2020-05-11T11:14:31 - COOLING_DOWN - refactor: Code cleanup in test_utils.py
 2020-05-13T11:05:48 - COOLING_DOWN - chore: Update dependencies
+2020-05-16T12:41:36 - COOLING_DOWN - refactor: Code cleanup in test_utils.py
