e2edaa74ac9e731a4db3e16186348133362d7f68
sam-hajj <sam@aceaicompany.com>
2020-06-10 11:14:04 +0000

    style: Apply linter fixes across core

--- src/core/utils.py
+++ src/core/utils.py
@@ -51,3 +51,4 @@
 2020-05-21T12:46:55 - COOLING_DOWN - fix: Address minor issues from backlog (related to utils.py)
 2020-05-26T14:49:58 - COOLING_DOWN - style: Apply linter fixes across core
 2020-05-28T11:11:56 - COOLING_DOWN - refactor: Code cleanup in utils.py
+2020-06-10T11:14:04 - COOLING_DOWN - style: Apply linter fixes across core
