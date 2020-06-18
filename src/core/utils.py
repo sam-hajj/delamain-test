56f1b7c4003b4caa5dc5e351cbda4aea8b03a096
sam-hajj <sam@aceaicompany.com>
2020-06-18 13:27:15 +0000

    feat: Add core logic for login service in utils.py

--- src/core/utils.py
+++ src/core/utils.py
@@ -56,3 +56,4 @@
 2020-06-11T14:32:29 - COOLING_DOWN - refactor: Code cleanup in utils.py
 2020-06-17T10:04:37 - RAMPING_UP - feat: Add core logic for caching service in utils.py
 2020-06-18T13:19:00 - RAMPING_UP - docs: Detail setup instructions
+2020-06-18T13:27:15 - RAMPING_UP - feat: Add core logic for login service in utils.py
