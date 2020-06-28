1fbc48778a9ea939b0e5ea69b79b5858430d1e2b
sam-hajj <sam@aceaicompany.com>
2020-06-28 09:22:07 +0000

    chore: Update requirements.txt

--- src/core/utils.py
+++ src/core/utils.py
@@ -58,3 +58,4 @@
 2020-06-18T13:19:00 - RAMPING_UP - docs: Detail setup instructions
 2020-06-18T13:27:15 - RAMPING_UP - feat: Add core logic for login service in utils.py
 2020-06-23T12:05:19 - RAMPING_UP - refactor: Improve reporting structure in core
+2020-06-28T09:22:07 - RAMPING_UP - chore: Update requirements.txt
