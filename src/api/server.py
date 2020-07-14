4ed239a55622700644bb4b95ed2d6a5ed8e8d872
sam-hajj <sam@aceaicompany.com>
2020-07-14 15:14:20 +0000

    test: Add initial unit tests for server.py

--- src/api/server.py
+++ src/api/server.py
@@ -40,3 +40,4 @@
 2020-07-08T12:44:38 - RAMPING_UP - feat: Implement basic data processing in server.py
 2020-07-08T13:27:05 - RAMPING_UP - refactor: Improve inventory structure in api
 2020-07-14T14:51:51 - RAMPING_UP - docs: Detail setup instructions
+2020-07-14T15:14:20 - RAMPING_UP - test: Add initial unit tests for server.py
