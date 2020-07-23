52daa0f5a601f52d5a4de9e6acb83c746c7d425c
sam-hajj <sam@aceaicompany.com>
2020-07-23 09:40:24 +0000

    fix: Resolve bug #646 in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,2 +1,3 @@
 2020-07-23T09:14:29 - SUSTAINED_PEAK - fix: Resolve bug #451 in auth.py
 2020-07-23T09:38:50 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
+2020-07-23T09:40:24 - SUSTAINED_PEAK - fix: Resolve bug #646 in auth.py
