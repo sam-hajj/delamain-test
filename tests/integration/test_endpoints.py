abb0314e17d74c8e9327dd6fc5ce0b6908694251
sam-hajj <sam@aceaicompany.com>
2020-07-25 11:24:31 +0000

    feat: Implement profile endpoint in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -1,2 +1,3 @@
 2020-07-25T11:20:25 - SUSTAINED_PEAK - feat!: Major feature integration: login affecting integration
 2020-07-25T11:23:14 - SUSTAINED_PEAK - refactor: Optimize pathfinding in test_endpoints.py
+2020-07-25T11:24:31 - SUSTAINED_PEAK - feat: Implement profile endpoint in test_endpoints.py
