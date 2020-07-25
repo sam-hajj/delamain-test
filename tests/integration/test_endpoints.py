38ae4179b1fff74a989e2c8988925b85cd01d173
sam-hajj <sam@aceaicompany.com>
2020-07-25 11:23:14 +0000

    refactor: Optimize pathfinding in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -1,1 +1,2 @@
 2020-07-25T11:20:25 - SUSTAINED_PEAK - feat!: Major feature integration: login affecting integration
+2020-07-25T11:23:14 - SUSTAINED_PEAK - refactor: Optimize pathfinding in test_endpoints.py
