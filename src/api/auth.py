2e7dee9a472b65660e09bc8e7fc0a1d2e1b8bf68
sam-hajj <sam@aceaicompany.com>
2020-07-27 14:36:00 +0000

    test: Increase coverage for reporting tests

--- src/api/auth.py
+++ src/api/auth.py
@@ -2,3 +2,4 @@
 2020-07-23T09:38:50 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
 2020-07-23T09:40:24 - SUSTAINED_PEAK - fix: Resolve bug #646 in auth.py
 2020-07-27T12:27:20 - SUSTAINED_PEAK - ci: Configure deployment pipeline in auth.py
+2020-07-27T14:36:00 - SUSTAINED_PEAK - test: Increase coverage for reporting tests
