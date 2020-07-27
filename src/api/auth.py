8adbefcfe999bf9ce3734b019627d0ce933a5851
sam-hajj <sam@aceaicompany.com>
2020-07-27 12:27:20 +0000

    ci: Configure deployment pipeline in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,3 +1,4 @@
 2020-07-23T09:14:29 - SUSTAINED_PEAK - fix: Resolve bug #451 in auth.py
 2020-07-23T09:38:50 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
 2020-07-23T09:40:24 - SUSTAINED_PEAK - fix: Resolve bug #646 in auth.py
+2020-07-27T12:27:20 - SUSTAINED_PEAK - ci: Configure deployment pipeline in auth.py
