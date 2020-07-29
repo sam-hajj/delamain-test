802979409bf2360527311317472540aeaa0828cc
sam-hajj <sam@aceaicompany.com>
2020-07-29 08:24:08 +0000

    test: Increase coverage for reporting tests

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -2,3 +2,4 @@
 2020-07-25T11:23:14 - SUSTAINED_PEAK - refactor: Optimize pathfinding in test_endpoints.py
 2020-07-25T11:24:31 - SUSTAINED_PEAK - feat: Implement profile endpoint in test_endpoints.py
 2020-07-28T11:56:19 - SUSTAINED_PEAK - feat: Implement data processing endpoint in test_endpoints.py
+2020-07-29T08:24:08 - SUSTAINED_PEAK - test: Increase coverage for reporting tests
