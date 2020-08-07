2420bc7adcbbe22bed55df5c4a47fd671284f36c
sam-hajj <sam@aceaicompany.com>
2020-08-07 13:34:35 +0000

    refactor: Optimize sorting in routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -1,3 +1,4 @@
 2020-07-27T16:03:35 - SUSTAINED_PEAK - feat!: Major feature integration: profile affecting api
 2020-08-04T11:08:22 - SUSTAINED_PEAK - test: Increase coverage for order tests
 2020-08-05T12:03:41 - SUSTAINED_PEAK - refactor: Optimize sorting in routes.py
+2020-08-07T13:34:35 - SUSTAINED_PEAK - refactor: Optimize sorting in routes.py
