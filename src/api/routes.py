8f43170f9a6c7c4c3116df12c2500e70116d26f0
sam-hajj <sam@aceaicompany.com>
2020-08-13 15:52:51 +0000

    feat: Implement payments endpoint in routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -3,3 +3,4 @@
 2020-08-05T12:03:41 - SUSTAINED_PEAK - refactor: Optimize sorting in routes.py
 2020-08-07T13:34:35 - SUSTAINED_PEAK - refactor: Optimize sorting in routes.py
 2020-08-08T08:08:51 - SUSTAINED_PEAK - feat!: Major feature integration: payments affecting api
+2020-08-13T15:52:51 - SUSTAINED_PEAK - feat: Implement payments endpoint in routes.py
