286bcf1f3de139af316cd8335984c1e85ef9dfe2
sam-hajj <sam@aceaicompany.com>
2020-08-14 14:02:58 +0000

    fix!: Critical patch for vulnerability in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -10,3 +10,4 @@
 2020-08-10T12:19:36 - SUSTAINED_PEAK - feat!: Major feature integration: data processing affecting integration
 2020-08-13T15:36:39 - SUSTAINED_PEAK - refactor: Optimize hashing in test_endpoints.py
 2020-08-13T15:44:02 - SUSTAINED_PEAK - feat: Implement profile endpoint in test_endpoints.py
+2020-08-14T14:02:58 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in test_endpoints.py
