ddd1796ad4cb05be3c1bc978d52f0940d83cc8bf
sam-hajj <sam@aceaicompany.com>
2020-08-15 12:07:32 +0000

    perf: Improve query performance in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -12,3 +12,4 @@
 2020-08-13T15:44:02 - SUSTAINED_PEAK - feat: Implement profile endpoint in test_endpoints.py
 2020-08-14T14:02:58 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in test_endpoints.py
 2020-08-14T14:51:39 - SUSTAINED_PEAK - refactor: Optimize parsing in test_endpoints.py
+2020-08-15T12:07:32 - SUSTAINED_PEAK - perf: Improve query performance in test_endpoints.py
