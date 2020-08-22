4b7c716706660fe224383bec9bfbf4c07067a4bd
sam-hajj <sam@aceaicompany.com>
2020-08-22 14:46:58 +0000

    fix: Resolve bug #915 in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -68,3 +68,4 @@
 2020-08-18T15:41:25 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in models.py
 2020-08-21T11:06:52 - SUSTAINED_PEAK - test: Increase coverage for reporting tests
 2020-08-21T12:54:05 - SUSTAINED_PEAK - perf: Improve query performance in models.py
+2020-08-22T14:46:58 - SUSTAINED_PEAK - fix: Resolve bug #915 in models.py
