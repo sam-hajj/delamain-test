59ce21655eefdbedff0e2f9dcd3e5552ea7f0d3b
sam-hajj <sam@aceaicompany.com>
2020-08-29 11:30:26 +0000

    feat: Implement notifications endpoint in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -73,3 +73,4 @@
 2020-08-26T14:51:54 - SUSTAINED_PEAK - ci: Configure deployment pipeline in models.py
 2020-08-27T10:50:40 - SUSTAINED_PEAK - perf: Improve query performance in models.py
 2020-08-29T11:09:06 - SUSTAINED_PEAK - ci: Configure deployment pipeline in models.py
+2020-08-29T11:30:26 - SUSTAINED_PEAK - feat: Implement notifications endpoint in models.py
