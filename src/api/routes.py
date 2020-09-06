91e63809f0cbf0d3b48613ecdca6c868389d034a
sam-hajj <sam@aceaicompany.com>
2020-09-06 13:53:21 +0000

    fix!: Critical patch for vulnerability in routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -17,3 +17,4 @@
 2020-09-02T13:51:46 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in routes.py
 2020-09-02T14:55:11 - SUSTAINED_PEAK - refactor: Optimize parsing in routes.py
 2020-09-03T12:20:47 - SUSTAINED_PEAK - refactor: Optimize parsing in routes.py
+2020-09-06T13:53:21 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in routes.py
