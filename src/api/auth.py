cba15906115ccc85fa32c4ae1febe81d91329f77
sam-hajj <sam@aceaicompany.com>
2020-09-11 15:48:26 +0000

    ci: Configure deployment pipeline in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -32,3 +32,4 @@
 2020-09-07T12:37:51 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
 2020-09-09T13:26:29 - SUSTAINED_PEAK - feat!: Major feature integration: caching affecting api
 2020-09-09T13:45:49 - SUSTAINED_PEAK - test: Increase coverage for user tests
+2020-09-11T15:48:26 - SUSTAINED_PEAK - ci: Configure deployment pipeline in auth.py
