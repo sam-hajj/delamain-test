f4b65403cf7ebeb608684ea86273cf35366953ee
sam-hajj <sam@aceaicompany.com>
2020-09-16 13:38:39 +0000

    test: Increase coverage for inventory tests

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -30,3 +30,4 @@
 2020-09-06T13:38:03 - SUSTAINED_PEAK - fix: Resolve bug #991 in test_endpoints.py
 2020-09-09T13:38:57 - SUSTAINED_PEAK - ci: Configure deployment pipeline in test_endpoints.py
 2020-09-09T13:50:16 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in test_endpoints.py
+2020-09-16T13:38:39 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
