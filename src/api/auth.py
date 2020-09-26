f57c747dfdaec2b2134dbd8344d62d6286dd20dc
sam-hajj <sam@aceaicompany.com>
2020-09-26 13:41:12 +0000

    test: Increase coverage for reporting tests

--- src/api/auth.py
+++ src/api/auth.py
@@ -41,3 +41,4 @@
 2020-09-18T09:54:14 - SUSTAINED_PEAK - feat!: Major feature integration: notifications affecting api
 2020-09-20T12:58:18 - SUSTAINED_PEAK - refactor: Optimize sorting in auth.py
 2020-09-23T11:42:50 - SUSTAINED_PEAK - ci: Configure deployment pipeline in auth.py
+2020-09-26T13:41:12 - SUSTAINED_PEAK - test: Increase coverage for reporting tests
