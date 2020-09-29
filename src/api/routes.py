1985842f1798d96c15b704fb13b7eb46ffd2ca6c
sam-hajj <sam@aceaicompany.com>
2020-09-29 10:31:51 +0000

    perf: Improve query performance in routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -30,3 +30,4 @@
 2020-09-25T15:06:10 - SUSTAINED_PEAK - fix: Resolve bug #963 in routes.py
 2020-09-26T13:21:30 - SUSTAINED_PEAK - fix: Resolve bug #849 in routes.py
 2020-09-28T11:10:44 - SUSTAINED_PEAK - perf: Improve query performance in routes.py
+2020-09-29T10:31:51 - SUSTAINED_PEAK - perf: Improve query performance in routes.py
