d349f5123688559bd926f48eba1605b708c216d2
sam-hajj <sam@aceaicompany.com>
2020-10-22 13:56:11 +0000

    docs: Update architecture diagram

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -1,3 +1,4 @@
 2020-10-21T17:09:10 - PROJECT_SHIFT - chore: Experiment with django in test_api.py
 2020-10-22T13:47:32 - PROJECT_SHIFT - refactor: Adapt user for new requirements in test_api.py
 2020-10-22T13:50:33 - PROJECT_SHIFT - chore: Experiment with pandas in test_api.py
+2020-10-22T13:56:11 - PROJECT_SHIFT - docs: Update architecture diagram
