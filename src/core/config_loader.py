f7f56dcc99bb54943daefb0226980a2718b90dc0
sam-hajj <sam@aceaicompany.com>
2020-10-24 15:59:30 +0000

    refactor: Modularize messaging queue in config_loader.py

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -1,3 +1,4 @@
 2020-10-20T13:12:37 - PROJECT_SHIFT - refactor: Modularize UI in config_loader.py
 2020-10-22T12:58:05 - PROJECT_SHIFT - refactor: Modularize messaging queue in config_loader.py
 2020-10-24T14:42:04 - PROJECT_SHIFT - docs: Update architecture diagram
+2020-10-24T15:59:30 - PROJECT_SHIFT - refactor: Modularize messaging queue in config_loader.py
