50cdc304f38c38bc7030cb53ead3ceaeee0de6a7
sam-hajj <sam@aceaicompany.com>
2020-10-26 12:49:02 +0000

    docs: Update architecture diagram

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -2,3 +2,4 @@
 2020-10-22T12:58:05 - PROJECT_SHIFT - refactor: Modularize messaging queue in config_loader.py
 2020-10-24T14:42:04 - PROJECT_SHIFT - docs: Update architecture diagram
 2020-10-24T15:59:30 - PROJECT_SHIFT - refactor: Modularize messaging queue in config_loader.py
+2020-10-26T12:49:02 - PROJECT_SHIFT - docs: Update architecture diagram
