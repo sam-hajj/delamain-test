a170ae364d2584a45a8a938ce1241d6dbd3e8a4e
sam-hajj <sam@aceaicompany.com>
2020-10-26 12:23:33 +0000

    refactor: Adapt inventory for new requirements in test_api.py

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -3,3 +3,4 @@
 2020-10-22T13:50:33 - PROJECT_SHIFT - chore: Experiment with pandas in test_api.py
 2020-10-22T13:56:11 - PROJECT_SHIFT - docs: Update architecture diagram
 2020-10-24T14:58:52 - PROJECT_SHIFT - refactor: Adapt product for new requirements in test_api.py
+2020-10-26T12:23:33 - PROJECT_SHIFT - refactor: Adapt inventory for new requirements in test_api.py
