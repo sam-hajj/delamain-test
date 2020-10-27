c72028ecf55475f416853b7aa43baf95f690d469
sam-hajj <sam@aceaicompany.com>
2020-10-27 09:05:52 +0000

    chore: Experiment with vue in config_loader.py

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -3,3 +3,4 @@
 2020-10-24T14:42:04 - PROJECT_SHIFT - docs: Update architecture diagram
 2020-10-24T15:59:30 - PROJECT_SHIFT - refactor: Modularize messaging queue in config_loader.py
 2020-10-26T12:49:02 - PROJECT_SHIFT - docs: Update architecture diagram
+2020-10-27T09:05:52 - PROJECT_SHIFT - chore: Experiment with vue in config_loader.py
