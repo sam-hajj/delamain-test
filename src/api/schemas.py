2b3ad0dc3eb0384c888c37d6f7c2fc7be24899f6
sam-hajj <sam@aceaicompany.com>
2020-10-27 10:28:46 +0000

    docs: Update architecture diagram

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,2 +1,3 @@
 2020-10-22T13:55:14 - PROJECT_SHIFT - feat: POC for real-time updates in api
 2020-10-26T12:28:04 - PROJECT_SHIFT - refactor: Adapt inventory for new requirements in schemas.py
+2020-10-27T10:28:46 - PROJECT_SHIFT - docs: Update architecture diagram
