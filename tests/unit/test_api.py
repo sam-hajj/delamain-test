2232b349b02bda70d40a25c82cdd31f486801377
sam-hajj <sam@aceaicompany.com>
2020-10-27 10:17:21 +0000

    test: Add integration tests for real-time updates

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -4,3 +4,4 @@
 2020-10-22T13:56:11 - PROJECT_SHIFT - docs: Update architecture diagram
 2020-10-24T14:58:52 - PROJECT_SHIFT - refactor: Adapt product for new requirements in test_api.py
 2020-10-26T12:23:33 - PROJECT_SHIFT - refactor: Adapt inventory for new requirements in test_api.py
+2020-10-27T10:17:21 - PROJECT_SHIFT - test: Add integration tests for real-time updates
