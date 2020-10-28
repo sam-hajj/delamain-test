69031a4f497ec54474c2573af1c29d68f6af715c
sam-hajj <sam@aceaicompany.com>
2020-10-28 14:33:41 +0000

    refactor: Modularize backend in schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,3 +1,4 @@
 2020-10-22T13:55:14 - PROJECT_SHIFT - feat: POC for real-time updates in api
 2020-10-26T12:28:04 - PROJECT_SHIFT - refactor: Adapt inventory for new requirements in schemas.py
 2020-10-27T10:28:46 - PROJECT_SHIFT - docs: Update architecture diagram
+2020-10-28T14:33:41 - PROJECT_SHIFT - refactor: Modularize backend in schemas.py
