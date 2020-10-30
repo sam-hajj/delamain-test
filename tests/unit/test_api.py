a2d3835a467e4da87083104b898896417fd8f3ee
sam-hajj <sam@aceaicompany.com>
2020-10-30 15:11:16 +0000

    chore: Experiment with requests in test_api.py

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -7,3 +7,4 @@
 2020-10-27T10:17:21 - PROJECT_SHIFT - test: Add integration tests for real-time updates
 2020-10-29T11:21:34 - PROJECT_SHIFT - docs: Update architecture diagram
 2020-10-29T14:28:15 - PROJECT_SHIFT - test: Add integration tests for analytics dashboard
+2020-10-30T15:11:16 - PROJECT_SHIFT - chore: Experiment with requests in test_api.py
