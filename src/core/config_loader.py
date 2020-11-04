998fff2c482a2295184d953da666b58d36d74ee2
sam-hajj <sam@aceaicompany.com>
2020-11-04 16:34:43 +0000

    refactor: Adapt reporting for new requirements in config_loader.py

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -9,3 +9,4 @@
 2020-10-29T13:32:34 - PROJECT_SHIFT - test: Add integration tests for analytics dashboard
 2020-11-02T15:42:02 - PROJECT_SHIFT - refactor: Modularize database in config_loader.py
 2020-11-03T10:51:22 - PROJECT_SHIFT - chore: Experiment with fastapi in config_loader.py
+2020-11-04T16:34:43 - PROJECT_SHIFT - refactor: Adapt reporting for new requirements in config_loader.py
