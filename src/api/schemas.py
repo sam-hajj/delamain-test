6dc12c733cd52d3372075a0e67662abc94d24e63
sam-hajj <sam@aceaicompany.com>
2020-11-06 13:23:14 +0000

    feat: POC for analytics dashboard in api

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -11,3 +11,4 @@
 2020-11-03T10:44:50 - PROJECT_SHIFT - docs: Update architecture diagram
 2020-11-04T18:45:31 - PROJECT_SHIFT - docs: Update architecture diagram
 2020-11-06T13:19:13 - PROJECT_SHIFT - test: Add integration tests for A/B testing framework
+2020-11-06T13:23:14 - PROJECT_SHIFT - feat: POC for analytics dashboard in api
