7e7969f3f2b6e2972428b0437bb04bef6308e15d
sam-hajj <sam@aceaicompany.com>
2020-11-08 13:04:30 +0000

    refactor: Modularize API gateway in config_loader.py

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -12,3 +12,4 @@
 2020-11-04T16:34:43 - PROJECT_SHIFT - refactor: Adapt reporting for new requirements in config_loader.py
 2020-11-04T16:53:30 - PROJECT_SHIFT - refactor: Modularize backend in config_loader.py
 2020-11-07T07:39:16 - PROJECT_SHIFT - refactor: Adapt auth for new requirements in config_loader.py
+2020-11-08T13:04:30 - PROJECT_SHIFT - refactor: Modularize API gateway in config_loader.py
