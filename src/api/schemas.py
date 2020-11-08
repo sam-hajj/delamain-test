53e149c3c18f9ea907cc0721937fa350df84f91c
sam-hajj <sam@aceaicompany.com>
2020-11-08 13:15:38 +0000

    docs: Update architecture diagram

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -13,3 +13,4 @@
 2020-11-06T13:19:13 - PROJECT_SHIFT - test: Add integration tests for A/B testing framework
 2020-11-06T13:23:14 - PROJECT_SHIFT - feat: POC for analytics dashboard in api
 2020-11-06T13:30:57 - PROJECT_SHIFT - refactor: Modularize UI in schemas.py
+2020-11-08T13:15:38 - PROJECT_SHIFT - docs: Update architecture diagram
