91cdcc8cef7c6ee7b63634ba10f9e0c3c944b7d7
sam-hajj <sam@aceaicompany.com>
2020-11-12 15:44:39 +0000

    docs: Update architecture diagram

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -24,3 +24,4 @@
 2020-11-07T09:50:02 - PROJECT_SHIFT - chore: Experiment with django in test_api.py
 2020-11-10T13:48:45 - PROJECT_SHIFT - test: Add integration tests for machine learning model
 2020-11-12T15:02:36 - PROJECT_SHIFT - feat: POC for machine learning model in unit
+2020-11-12T15:44:39 - PROJECT_SHIFT - docs: Update architecture diagram
