159a8ec7659c5f0a7c116e5de5c55a9cd63b1147
sam-hajj <sam@aceaicompany.com>
2020-11-23 14:20:38 +0000

    refactor: Adapt product for new requirements in schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -25,3 +25,4 @@
 2020-11-18T11:36:22 - PROJECT_SHIFT - feat: POC for A/B testing framework in api
 2020-11-20T13:52:27 - PROJECT_SHIFT - chore: Experiment with fastapi in schemas.py
 2020-11-23T14:04:15 - PROJECT_SHIFT - chore: Experiment with requests in schemas.py
+2020-11-23T14:20:38 - PROJECT_SHIFT - refactor: Adapt product for new requirements in schemas.py
