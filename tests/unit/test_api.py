544aa8da774f0d74622c73b715e12977fea28bb3
sam-hajj <sam@aceaicompany.com>
2020-11-29 13:39:20 +0000

    refactor: Adapt product for new requirements in test_api.py

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -29,3 +29,4 @@
 2020-11-17T12:58:14 - PROJECT_SHIFT - test: Add integration tests for machine learning model
 2020-11-20T13:00:46 - PROJECT_SHIFT - test: Add integration tests for machine learning model
 2020-11-24T13:22:36 - PROJECT_SHIFT - docs: Update architecture diagram
+2020-11-29T13:39:20 - PROJECT_SHIFT - refactor: Adapt product for new requirements in test_api.py
