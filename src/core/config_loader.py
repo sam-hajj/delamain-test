6bc7cbf14b4245724a546ea25e86118671d0f9a1
sam-hajj <sam@aceaicompany.com>
2020-12-07 10:56:45 +0000

    refactor: Adapt user for new requirements in config_loader.py

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -23,3 +23,4 @@
 2020-12-01T12:38:14 - PROJECT_SHIFT - refactor: Adapt user for new requirements in config_loader.py
 2020-12-04T10:55:09 - PROJECT_SHIFT - refactor: Adapt user for new requirements in config_loader.py
 2020-12-06T15:06:05 - PROJECT_SHIFT - refactor: Modularize UI in config_loader.py
+2020-12-07T10:56:45 - PROJECT_SHIFT - refactor: Adapt user for new requirements in config_loader.py
