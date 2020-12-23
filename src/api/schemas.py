7027292f98c50a4c2ec3304a2af04a0b196d7a74
sam-hajj <sam@aceaicompany.com>
2020-12-23 10:14:44 +0000

    refactor: Modularize backend in schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -39,3 +39,4 @@
 2020-12-15T14:48:13 - PROJECT_SHIFT - docs: Update architecture diagram
 2020-12-17T10:29:14 - PROJECT_SHIFT - test: Add integration tests for real-time updates
 2020-12-22T15:44:04 - PROJECT_SHIFT - docs: Update architecture diagram
+2020-12-23T10:14:44 - PROJECT_SHIFT - refactor: Modularize backend in schemas.py
