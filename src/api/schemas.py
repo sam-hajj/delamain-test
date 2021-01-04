4e6ac0dcd3e21dce080de1765a0cdea2168d033c
sam-hajj <sam@aceaicompany.com>
2021-01-04 12:17:07 +0000

    chore: Experiment with requests in schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -48,3 +48,4 @@
 2021-01-02T14:24:09 - PROJECT_SHIFT - refactor: Modularize database in schemas.py
 2021-01-03T14:16:59 - PROJECT_SHIFT - test: Add integration tests for analytics dashboard
 2021-01-04T12:17:02 - PROJECT_SHIFT - refactor: Modularize backend in schemas.py
+2021-01-04T12:17:07 - PROJECT_SHIFT - chore: Experiment with requests in schemas.py
