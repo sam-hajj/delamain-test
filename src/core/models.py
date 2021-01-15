8f3a8c79b7a80a5120d303f6a1ce0050e1c3100a
sam-hajj <sam@aceaicompany.com>
2021-01-15 12:43:39 +0000

    feat: Implement basic search in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -110,3 +110,4 @@
 2021-01-09T12:46:19 - RAMPING_UP - chore: Update requirements.txt
 2021-01-09T14:38:50 - RAMPING_UP - feat: Add core logic for notifications service in models.py
 2021-01-15T11:40:49 - RAMPING_UP - chore: Update requirements.txt
+2021-01-15T12:43:39 - RAMPING_UP - feat: Implement basic search in models.py
