0c6dfc9acf86d1ba2ea8e4989244a3a224c0cc78
sam-hajj <sam@aceaicompany.com>
2021-01-18 11:23:53 +0000

    refactor: Improve product structure in api

--- src/api/server.py
+++ src/api/server.py
@@ -50,3 +50,4 @@
 2021-01-14T13:28:07 - RAMPING_UP - chore: Update requirements.txt
 2021-01-17T12:54:33 - RAMPING_UP - chore: Update requirements.txt
 2021-01-18T11:08:58 - RAMPING_UP - docs: Detail setup instructions
+2021-01-18T11:23:53 - RAMPING_UP - refactor: Improve product structure in api
