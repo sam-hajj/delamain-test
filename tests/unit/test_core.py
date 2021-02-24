8fc660f6de74f9fb61f213b7e7dda49307f7442a
sam-hajj <sam@aceaicompany.com>
2021-02-24 13:01:16 +0000

    refactor: Improve product structure in unit

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -69,3 +69,4 @@
 2021-02-15T14:49:27 - RAMPING_UP - feat: Add core logic for notifications service in test_core.py
 2021-02-18T09:13:23 - RAMPING_UP - feat: Implement basic search in test_core.py
 2021-02-18T10:45:53 - RAMPING_UP - refactor: Improve inventory structure in unit
+2021-02-24T13:01:16 - RAMPING_UP - refactor: Improve product structure in unit
