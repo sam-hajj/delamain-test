e93a5fada52c4fc8a059787edadb3f1e69fb30f2
sam-hajj <sam@aceaicompany.com>
2021-02-28 14:11:28 +0000

    test: Add initial unit tests for models.py

--- src/core/models.py
+++ src/core/models.py
@@ -126,3 +126,4 @@
 2021-02-18T10:58:23 - RAMPING_UP - test: Add initial unit tests for models.py
 2021-02-24T14:09:32 - RAMPING_UP - refactor: Improve product structure in core
 2021-02-28T12:00:05 - RAMPING_UP - feat: Implement basic login in models.py
+2021-02-28T14:11:28 - RAMPING_UP - test: Add initial unit tests for models.py
