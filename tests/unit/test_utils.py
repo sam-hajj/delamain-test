eaae3c8771743c656d1e51c595547833db576a0c
sam-hajj <sam@aceaicompany.com>
2021-03-30 14:21:25 +0000

    style: Apply linter fixes across unit

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -42,3 +42,4 @@
 2021-03-23T11:47:36 - COOLING_DOWN - test: Refactor tests for inventory for clarity
 2021-03-24T14:17:41 - COOLING_DOWN - chore: Update dependencies
 2021-03-26T14:26:53 - COOLING_DOWN - chore: Update dependencies
+2021-03-30T14:21:25 - COOLING_DOWN - style: Apply linter fixes across unit
