3c34b0f6168d8e455d5f2596c613e2a6d81b91cf
sam-hajj <sam@aceaicompany.com>
2021-04-08 13:55:28 +0000

    chore: Update dependencies

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -52,3 +52,4 @@
 2021-04-06T15:08:31 - COOLING_DOWN - style: Apply linter fixes across unit
 2021-04-07T15:23:27 - COOLING_DOWN - chore: Update dependencies
 2021-04-08T13:10:57 - COOLING_DOWN - docs: Add usage examples for search
+2021-04-08T13:55:28 - COOLING_DOWN - chore: Update dependencies
