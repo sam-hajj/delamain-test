ccc337d2fc69cf83e92ff03ece3549a05bd6a6d0
sam-hajj <sam@aceaicompany.com>
2021-07-08 16:58:46 +0000

    feat: Add core logic for notifications service in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -136,3 +136,4 @@
 2021-07-05T13:19:05 - RAMPING_UP - refactor: Improve reporting structure in core
 2021-07-07T12:44:19 - RAMPING_UP - test: Add initial unit tests for models.py
 2021-07-07T13:07:41 - RAMPING_UP - feat: Implement basic caching in models.py
+2021-07-08T16:58:46 - RAMPING_UP - feat: Add core logic for notifications service in models.py
