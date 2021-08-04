31cfee2c36348b2b4f4414f62719a215d53107c0
sam-hajj <sam@aceaicompany.com>
2021-08-04 11:43:15 +0000

    refactor: Improve order structure in unit

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -80,3 +80,4 @@
 2021-07-26T14:56:51 - RAMPING_UP - refactor: Improve reporting structure in unit
 2021-07-28T13:45:31 - RAMPING_UP - feat: Add core logic for search service in test_core.py
 2021-07-29T15:48:28 - RAMPING_UP - feat: Implement basic caching in test_core.py
+2021-08-04T11:43:15 - RAMPING_UP - refactor: Improve order structure in unit
