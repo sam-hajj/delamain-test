49f6a1eb078b311fa020cfeea0929b20e09b2927
sam-hajj <sam@aceaicompany.com>
2021-08-22 10:44:18 +0000

    test: Add initial unit tests for server.py

--- src/api/server.py
+++ src/api/server.py
@@ -89,3 +89,4 @@
 2021-08-18T13:41:42 - RAMPING_UP - feat: Add core logic for notifications service in server.py
 2021-08-18T13:52:46 - RAMPING_UP - feat: Implement basic search in server.py
 2021-08-22T10:39:57 - RAMPING_UP - feat: Add core logic for profile service in server.py
+2021-08-22T10:44:18 - RAMPING_UP - test: Add initial unit tests for server.py
