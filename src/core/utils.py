4f015f8cd9f63d44f64a3539b0ad3f24a3cac759
sam-hajj <sam@aceaicompany.com>
2021-08-23 12:39:43 +0000

    docs: Detail setup instructions

--- src/core/utils.py
+++ src/core/utils.py
@@ -149,3 +149,4 @@
 2021-08-21T14:22:05 - RAMPING_UP - chore: Update requirements.txt
 2021-08-22T10:05:15 - RAMPING_UP - test: Add initial unit tests for utils.py
 2021-08-23T12:33:52 - RAMPING_UP - docs: Detail setup instructions
+2021-08-23T12:39:43 - RAMPING_UP - docs: Detail setup instructions
