a79f484d85ae1d267fc6ca5f4df6923cebb8a90b
sam-hajj <sam@aceaicompany.com>
2021-08-29 09:32:23 +0000

    fix!: Critical patch for vulnerability in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -59,3 +59,4 @@
 2021-08-27T13:06:24 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in test_endpoints.py
 2021-08-28T12:33:40 - SUSTAINED_PEAK - test: Increase coverage for user tests
 2021-08-29T09:22:36 - SUSTAINED_PEAK - refactor: Optimize sorting in test_endpoints.py
+2021-08-29T09:32:23 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in test_endpoints.py
