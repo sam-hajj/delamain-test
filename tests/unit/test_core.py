390e9c69464ae769aa442088b153ab4697daf2a8
sam-hajj <sam@aceaicompany.com>
2021-11-02 14:45:30 +0000

    refactor: Improve product structure in unit

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -97,3 +97,4 @@
 2021-10-30T12:17:57 - RAMPING_UP - docs: Detail setup instructions
 2021-10-30T12:55:34 - RAMPING_UP - feat: Implement basic profile in test_core.py
 2021-11-02T12:32:04 - RAMPING_UP - test: Add initial unit tests for test_core.py
+2021-11-02T14:45:30 - RAMPING_UP - refactor: Improve product structure in unit
