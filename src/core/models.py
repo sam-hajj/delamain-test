273ef4a7cbb3e2ee7e6c0520b7d8d9661907bc32
sam-hajj <sam@aceaicompany.com>
2021-11-09 13:47:10 +0000

    feat: Add core logic for login service in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -172,3 +172,4 @@
 2021-11-04T09:25:51 - RAMPING_UP - refactor: Improve product structure in core
 2021-11-05T15:29:54 - RAMPING_UP - feat: Add core logic for caching service in models.py
 2021-11-09T13:46:51 - RAMPING_UP - chore: Update requirements.txt
+2021-11-09T13:47:10 - RAMPING_UP - feat: Add core logic for login service in models.py
