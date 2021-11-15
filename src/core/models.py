52261b5758c2a9cf10bffb94746b1481ec82de59
sam-hajj <sam@aceaicompany.com>
2021-11-15 10:12:39 +0000

    feat: Implement basic payments in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -175,3 +175,4 @@
 2021-11-09T13:47:10 - RAMPING_UP - feat: Add core logic for login service in models.py
 2021-11-10T11:20:41 - RAMPING_UP - test: Add initial unit tests for models.py
 2021-11-10T11:54:56 - RAMPING_UP - refactor: Improve reporting structure in core
+2021-11-15T10:12:39 - RAMPING_UP - feat: Implement basic payments in models.py
