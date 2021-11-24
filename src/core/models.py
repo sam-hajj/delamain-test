f25a37c40c73954681692f321fe91675d4919435
sam-hajj <sam@aceaicompany.com>
2021-11-24 10:48:55 +0000

    chore: Update requirements.txt

--- src/core/models.py
+++ src/core/models.py
@@ -181,3 +181,4 @@
 2021-11-18T12:24:44 - RAMPING_UP - feat: Add core logic for caching service in models.py
 2021-11-23T10:33:29 - RAMPING_UP - test: Add initial unit tests for models.py
 2021-11-24T10:03:29 - RAMPING_UP - docs: Detail setup instructions
+2021-11-24T10:48:55 - RAMPING_UP - chore: Update requirements.txt
