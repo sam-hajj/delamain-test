b8d6338a0a8f15b7dada2387b25cbe46aa8829d6
sam-hajj <sam@aceaicompany.com>
2021-12-02 12:03:10 +0000

    refactor: Improve inventory structure in api

--- src/api/server.py
+++ src/api/server.py
@@ -106,3 +106,4 @@
 2021-11-25T12:46:21 - RAMPING_UP - feat: Add core logic for profile service in server.py
 2021-11-25T12:59:26 - RAMPING_UP - docs: Detail setup instructions
 2021-12-01T15:30:04 - RAMPING_UP - feat: Implement basic search in server.py
+2021-12-02T12:03:10 - RAMPING_UP - refactor: Improve inventory structure in api
