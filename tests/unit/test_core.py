74efde8cd2a6269ccf615d3c0dd81a6a765d819c
sam-hajj <sam@aceaicompany.com>
2021-12-07 09:49:30 +0000

    chore: Update requirements.txt

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -109,3 +109,4 @@
 2021-11-24T11:42:39 - RAMPING_UP - refactor: Improve auth structure in unit
 2021-12-01T15:45:35 - RAMPING_UP - feat: Add core logic for profile service in test_core.py
 2021-12-02T11:24:54 - RAMPING_UP - refactor: Improve reporting structure in unit
+2021-12-07T09:49:30 - RAMPING_UP - chore: Update requirements.txt
