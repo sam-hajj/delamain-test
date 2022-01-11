04424559f4a74276c63a0ce8436a4cc493357e14
sam-hajj <sam@aceaicompany.com>
2022-01-11 12:28:21 +0000

    fix: Resolve bug #688 in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -77,3 +77,4 @@
 2022-01-08T15:55:26 - SUSTAINED_PEAK - fix: Resolve bug #400 in test_endpoints.py
 2022-01-10T12:48:22 - SUSTAINED_PEAK - feat: Implement profile endpoint in test_endpoints.py
 2022-01-11T12:01:15 - SUSTAINED_PEAK - ci: Configure deployment pipeline in test_endpoints.py
+2022-01-11T12:28:21 - SUSTAINED_PEAK - fix: Resolve bug #688 in test_endpoints.py
