d308784526f601b836738c230f419b144a34f748
sam-hajj <sam@aceaicompany.com>
2022-01-12 12:51:00 +0000

    test: Increase coverage for inventory tests

--- src/api/routes.py
+++ src/api/routes.py
@@ -64,3 +64,4 @@
 2022-01-10T13:47:51 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in routes.py
 2022-01-11T12:56:13 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
 2022-01-12T12:26:30 - SUSTAINED_PEAK - ci: Configure deployment pipeline in routes.py
+2022-01-12T12:51:00 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
