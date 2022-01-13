fea1a479b95e77abe27a1b319189cbc189c2393f
sam-hajj <sam@aceaicompany.com>
2022-01-13 11:12:10 +0000

    perf: Improve query performance in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -80,3 +80,4 @@
 2022-01-11T12:28:21 - SUSTAINED_PEAK - fix: Resolve bug #688 in test_endpoints.py
 2022-01-11T12:55:15 - SUSTAINED_PEAK - ci: Configure deployment pipeline in test_endpoints.py
 2022-01-12T13:09:35 - SUSTAINED_PEAK - refactor: Optimize pathfinding in test_endpoints.py
+2022-01-13T11:12:10 - SUSTAINED_PEAK - perf: Improve query performance in test_endpoints.py
