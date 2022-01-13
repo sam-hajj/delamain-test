528fd6d659ba9a12bee879e66b44c649f76bbd3e
sam-hajj <sam@aceaicompany.com>
2022-01-13 11:39:45 +0000

    perf: Improve query performance in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -213,3 +213,4 @@
 2022-01-07T09:12:47 - SUSTAINED_PEAK - perf: Improve query performance in models.py
 2022-01-08T15:25:08 - SUSTAINED_PEAK - perf: Improve query performance in models.py
 2022-01-11T12:59:30 - SUSTAINED_PEAK - feat: Implement profile endpoint in models.py
+2022-01-13T11:39:45 - SUSTAINED_PEAK - perf: Improve query performance in models.py
