26b0acc0a847acd55088b2357f4658c41ad9f41a
sam-hajj <sam@aceaicompany.com>
2022-01-18 13:22:27 +0000

    perf: Improve query performance in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -76,3 +76,4 @@
 2022-01-13T11:32:14 - SUSTAINED_PEAK - perf: Improve query performance in auth.py
 2022-01-15T13:35:14 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in auth.py
 2022-01-15T13:59:03 - SUSTAINED_PEAK - fix: Resolve bug #265 in auth.py
+2022-01-18T13:22:27 - SUSTAINED_PEAK - perf: Improve query performance in auth.py
