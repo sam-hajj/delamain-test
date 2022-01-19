8e78d8583a87ae5807be9ac7500d161fca40c054
sam-hajj <sam@aceaicompany.com>
2022-01-19 13:16:48 +0000

    refactor: Optimize pathfinding in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -92,3 +92,4 @@
 2022-01-17T10:31:13 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in test_endpoints.py
 2022-01-17T10:46:12 - SUSTAINED_PEAK - test: Increase coverage for user tests
 2022-01-18T13:37:47 - SUSTAINED_PEAK - feat: Implement notifications endpoint in test_endpoints.py
+2022-01-19T13:16:48 - SUSTAINED_PEAK - refactor: Optimize pathfinding in test_endpoints.py
