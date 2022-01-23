734b3f62dcc036c25f561292192c145e10714fe6
sam-hajj <sam@aceaicompany.com>
2022-01-23 12:51:39 +0000

    feat: Implement notifications endpoint in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -97,3 +97,4 @@
 2022-01-20T11:01:51 - SUSTAINED_PEAK - fix: Resolve bug #760 in test_endpoints.py
 2022-01-21T11:24:56 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
 2022-01-23T12:14:47 - SUSTAINED_PEAK - feat: Implement caching endpoint in test_endpoints.py
+2022-01-23T12:51:39 - SUSTAINED_PEAK - feat: Implement notifications endpoint in test_endpoints.py
