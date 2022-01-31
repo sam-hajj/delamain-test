423be237876ad7ad4cc3e4c54cc7d9cbc8fda8b4
sam-hajj <sam@aceaicompany.com>
2022-01-31 09:06:51 +0000

    fix!: Critical patch for vulnerability in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -85,3 +85,4 @@
 2022-01-25T13:41:07 - SUSTAINED_PEAK - perf: Improve query performance in auth.py
 2022-01-27T14:28:52 - SUSTAINED_PEAK - test: Increase coverage for auth tests
 2022-01-31T09:05:09 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in auth.py
+2022-01-31T09:06:51 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in auth.py
