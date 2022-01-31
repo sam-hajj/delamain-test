c376a0fcb2a3aee07c29fe7b5c0cc3559c46c8d7
sam-hajj <sam@aceaicompany.com>
2022-01-31 08:19:00 +0000

    feat: Implement profile endpoint in main.py

--- src/core/main.py
+++ src/core/main.py
@@ -121,3 +121,4 @@
 2022-01-28T14:50:47 - SUSTAINED_PEAK - perf: Improve query performance in main.py
 2022-01-28T14:52:33 - SUSTAINED_PEAK - feat: Implement search endpoint in main.py
 2022-01-29T14:18:57 - SUSTAINED_PEAK - ci: Configure deployment pipeline in main.py
+2022-01-31T08:19:00 - SUSTAINED_PEAK - feat: Implement profile endpoint in main.py
