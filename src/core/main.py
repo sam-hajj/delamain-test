2c58b5225b10c0d912d94577c7f6ea9e93da51cf
sam-hajj <sam@aceaicompany.com>
2022-02-03 13:36:09 +0000

    fix!: Critical patch for vulnerability in main.py

--- src/core/main.py
+++ src/core/main.py
@@ -126,3 +126,4 @@
 2022-01-31T09:32:07 - SUSTAINED_PEAK - feat: Implement data processing endpoint in main.py
 2022-02-01T13:46:17 - SUSTAINED_PEAK - ci: Configure deployment pipeline in main.py
 2022-02-01T13:57:08 - SUSTAINED_PEAK - fix: Resolve bug #36 in main.py
+2022-02-03T13:36:09 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in main.py
