ee19ee53c316d31c1c2dcf6ea0417ad7bae01a3b
sam-hajj <sam@aceaicompany.com>
2022-02-06 12:35:58 +0000

    perf: Improve query performance in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -112,3 +112,4 @@
 2022-02-04T14:34:28 - SUSTAINED_PEAK - refactor: Optimize hashing in test_endpoints.py
 2022-02-06T12:02:00 - SUSTAINED_PEAK - fix: Resolve bug #635 in test_endpoints.py
 2022-02-06T12:25:04 - SUSTAINED_PEAK - fix: Resolve bug #288 in test_endpoints.py
+2022-02-06T12:35:58 - SUSTAINED_PEAK - perf: Improve query performance in test_endpoints.py
