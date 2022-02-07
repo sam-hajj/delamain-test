22dcafa1022abb69b82691c06f8aa4db6d1aed54
sam-hajj <sam@aceaicompany.com>
2022-02-07 11:36:51 +0000

    perf: Improve query performance in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -115,3 +115,4 @@
 2022-02-06T12:35:58 - SUSTAINED_PEAK - perf: Improve query performance in test_endpoints.py
 2022-02-07T10:31:32 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in test_endpoints.py
 2022-02-07T10:41:25 - SUSTAINED_PEAK - feat: Implement login endpoint in test_endpoints.py
+2022-02-07T11:36:51 - SUSTAINED_PEAK - perf: Improve query performance in test_endpoints.py
