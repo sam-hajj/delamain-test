c23b6a0d5b4205ab9ada16b1a260a1682e2e365d
sam-hajj <sam@aceaicompany.com>
2022-02-10 11:13:31 +0000

    feat: Implement payments endpoint in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -96,3 +96,4 @@
 2022-02-07T10:59:25 - SUSTAINED_PEAK - feat!: Major feature integration: login affecting api
 2022-02-08T12:16:48 - SUSTAINED_PEAK - feat!: Major feature integration: profile affecting api
 2022-02-08T12:36:24 - SUSTAINED_PEAK - refactor: Optimize parsing in auth.py
+2022-02-10T11:13:31 - SUSTAINED_PEAK - feat: Implement payments endpoint in auth.py
