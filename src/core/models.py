ed4aaf662a0ecdc0ce98e9dd20d8a485621cf5db
sam-hajj <sam@aceaicompany.com>
2022-02-10 11:00:44 +0000

    ci: Configure deployment pipeline in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -245,3 +245,4 @@
 2022-02-07T10:58:14 - SUSTAINED_PEAK - feat!: Major feature integration: login affecting core
 2022-02-08T12:38:54 - SUSTAINED_PEAK - ci: Configure deployment pipeline in models.py
 2022-02-10T10:54:07 - SUSTAINED_PEAK - ci: Configure deployment pipeline in models.py
+2022-02-10T11:00:44 - SUSTAINED_PEAK - ci: Configure deployment pipeline in models.py
