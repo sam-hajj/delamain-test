dc2c1f61b8fdd2265eb6cde1561dad33d912545b
sam-hajj <sam@aceaicompany.com>
2022-02-23 11:48:04 +0000

    feat!: Major feature integration: notifications affecting integration

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -129,3 +129,4 @@
 2022-02-18T12:39:24 - SUSTAINED_PEAK - test: Increase coverage for product tests
 2022-02-18T13:48:46 - SUSTAINED_PEAK - feat!: Major feature integration: login affecting integration
 2022-02-22T12:58:10 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in test_endpoints.py
+2022-02-23T11:48:04 - SUSTAINED_PEAK - feat!: Major feature integration: notifications affecting integration
