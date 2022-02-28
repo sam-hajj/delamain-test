abe02f39a0d1c8e98b27c322940175d7fa215b1f
sam-hajj <sam@aceaicompany.com>
2022-02-28 14:09:32 +0000

    ci: Configure deployment pipeline in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -258,3 +258,4 @@
 2022-02-24T15:01:01 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in models.py
 2022-02-24T15:33:27 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in models.py
 2022-02-26T13:57:47 - SUSTAINED_PEAK - ci: Configure deployment pipeline in models.py
+2022-02-28T14:09:32 - SUSTAINED_PEAK - ci: Configure deployment pipeline in models.py
