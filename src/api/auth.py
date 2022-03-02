3738e0097cb380bf5094e7370514e37ce0b4b319
sam-hajj <sam@aceaicompany.com>
2022-03-02 14:58:41 +0000

    feat: Implement login endpoint in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -110,3 +110,4 @@
 2022-02-26T13:02:43 - SUSTAINED_PEAK - feat!: Major feature integration: notifications affecting api
 2022-02-27T16:09:50 - SUSTAINED_PEAK - feat!: Major feature integration: caching affecting api
 2022-02-28T15:11:22 - SUSTAINED_PEAK - feat!: Major feature integration: notifications affecting api
+2022-03-02T14:58:41 - SUSTAINED_PEAK - feat: Implement login endpoint in auth.py
