18c4f2442b542f0c4e3eb7287f0a8313e140b7d5
sam-hajj <sam@aceaicompany.com>
2022-03-05 12:44:25 +0000

    test: Increase coverage for auth tests

--- src/core/main.py
+++ src/core/main.py
@@ -154,3 +154,4 @@
 2022-03-03T11:45:36 - SUSTAINED_PEAK - feat!: Major feature integration: payments affecting core
 2022-03-03T13:28:30 - SUSTAINED_PEAK - perf: Improve query performance in main.py
 2022-03-05T11:25:52 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in main.py
+2022-03-05T12:44:25 - SUSTAINED_PEAK - test: Increase coverage for auth tests
