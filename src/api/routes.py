570e93ef645dbcb09b975b7de289d163294a6f5e
sam-hajj <sam@aceaicompany.com>
2022-03-05 12:01:14 +0000

    fix: Resolve bug #805 in routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -122,3 +122,4 @@
 2022-03-02T13:38:13 - SUSTAINED_PEAK - feat!: Major feature integration: search affecting api
 2022-03-02T13:38:32 - SUSTAINED_PEAK - feat: Implement notifications endpoint in routes.py
 2022-03-02T13:53:03 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in routes.py
+2022-03-05T12:01:14 - SUSTAINED_PEAK - fix: Resolve bug #805 in routes.py
