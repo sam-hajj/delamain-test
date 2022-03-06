f66198f1373595ed863cd560d5eb497d32873e1b
sam-hajj <sam@aceaicompany.com>
2022-03-06 11:41:19 +0000

    test: Increase coverage for user tests

--- src/core/models.py
+++ src/core/models.py
@@ -268,3 +268,4 @@
 2022-03-03T11:54:52 - SUSTAINED_PEAK - perf: Improve query performance in models.py
 2022-03-05T11:10:32 - SUSTAINED_PEAK - ci: Configure deployment pipeline in models.py
 2022-03-05T12:21:36 - SUSTAINED_PEAK - perf: Improve query performance in models.py
+2022-03-06T11:41:19 - SUSTAINED_PEAK - test: Increase coverage for user tests
