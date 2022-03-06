fc66b044af4f79aea3600aa92085a17df7aff2b3
sam-hajj <sam@aceaicompany.com>
2022-03-06 11:58:09 +0000

    refactor: Optimize hashing in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -142,3 +142,4 @@
 2022-03-03T11:51:33 - SUSTAINED_PEAK - refactor: Optimize hashing in test_endpoints.py
 2022-03-05T12:00:29 - SUSTAINED_PEAK - test: Increase coverage for order tests
 2022-03-06T11:38:20 - SUSTAINED_PEAK - ci: Configure deployment pipeline in test_endpoints.py
+2022-03-06T11:58:09 - SUSTAINED_PEAK - refactor: Optimize hashing in test_endpoints.py
