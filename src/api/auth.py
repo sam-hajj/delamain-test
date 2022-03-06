db00999d7438c07940bb373654510635d7309e12
sam-hajj <sam@aceaicompany.com>
2022-03-06 11:15:21 +0000

    feat!: Major feature integration: notifications affecting api

--- src/api/auth.py
+++ src/api/auth.py
@@ -114,3 +114,4 @@
 2022-03-03T11:26:50 - SUSTAINED_PEAK - ci: Configure deployment pipeline in auth.py
 2022-03-05T11:23:48 - SUSTAINED_PEAK - refactor: Optimize pathfinding in auth.py
 2022-03-05T13:10:15 - SUSTAINED_PEAK - feat!: Major feature integration: caching affecting api
+2022-03-06T11:15:21 - SUSTAINED_PEAK - feat!: Major feature integration: notifications affecting api
