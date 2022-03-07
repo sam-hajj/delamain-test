a39adca626f03da010a08f4d5eacc742bcceaa8a
sam-hajj <sam@aceaicompany.com>
2022-03-07 12:28:48 +0000

    feat: Implement payments endpoint in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -270,3 +270,4 @@
 2022-03-05T12:21:36 - SUSTAINED_PEAK - perf: Improve query performance in models.py
 2022-03-06T11:41:19 - SUSTAINED_PEAK - test: Increase coverage for user tests
 2022-03-07T12:16:15 - SUSTAINED_PEAK - fix: Resolve bug #48 in models.py
+2022-03-07T12:28:48 - SUSTAINED_PEAK - feat: Implement payments endpoint in models.py
