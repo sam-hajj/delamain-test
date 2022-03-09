6e4d4998b572d0034b0b749c2ee2b0a02e5dfa89
sam-hajj <sam@aceaicompany.com>
2022-03-09 12:38:26 +0000

    fix: Resolve bug #164 in routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -131,3 +131,4 @@
 2022-03-08T13:28:51 - SUSTAINED_PEAK - ci: Configure deployment pipeline in routes.py
 2022-03-08T13:54:42 - SUSTAINED_PEAK - feat: Implement login endpoint in routes.py
 2022-03-09T12:21:19 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in routes.py
+2022-03-09T12:38:26 - SUSTAINED_PEAK - fix: Resolve bug #164 in routes.py
