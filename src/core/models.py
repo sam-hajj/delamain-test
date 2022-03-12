9675224fcc00dcb403ee2005e13063f645bad114
sam-hajj <sam@aceaicompany.com>
2022-03-12 12:44:54 +0000

    perf: Improve query performance in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -281,3 +281,4 @@
 2022-03-11T09:57:36 - SUSTAINED_PEAK - ci: Configure deployment pipeline in models.py
 2022-03-11T10:34:10 - SUSTAINED_PEAK - feat!: Major feature integration: payments affecting core
 2022-03-12T12:32:38 - SUSTAINED_PEAK - ci: Configure deployment pipeline in models.py
+2022-03-12T12:44:54 - SUSTAINED_PEAK - perf: Improve query performance in models.py
