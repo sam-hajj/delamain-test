ef27967879940219a1226d079ec17fb169e82738
sam-hajj <sam@aceaicompany.com>
2022-03-15 11:10:00 +0000

    refactor: Optimize hashing in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -125,3 +125,4 @@
 2022-03-12T12:55:39 - SUSTAINED_PEAK - test: Increase coverage for order tests
 2022-03-12T12:58:41 - SUSTAINED_PEAK - feat!: Major feature integration: notifications affecting api
 2022-03-15T10:55:34 - SUSTAINED_PEAK - test: Increase coverage for product tests
+2022-03-15T11:10:00 - SUSTAINED_PEAK - refactor: Optimize hashing in auth.py
