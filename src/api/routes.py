ba00ce36785d0f814f6e8e008c83829418370aa9
sam-hajj <sam@aceaicompany.com>
2022-03-17 13:44:03 +0000

    perf: Improve query performance in routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -140,3 +140,4 @@
 2022-03-15T10:28:54 - SUSTAINED_PEAK - ci: Configure deployment pipeline in routes.py
 2022-03-15T10:52:18 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in routes.py
 2022-03-17T12:21:35 - SUSTAINED_PEAK - refactor: Optimize hashing in routes.py
+2022-03-17T13:44:03 - SUSTAINED_PEAK - perf: Improve query performance in routes.py
