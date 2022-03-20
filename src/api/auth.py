2d7eb8a61e09cb9dc62cd4eb03690ea7d228aa24
sam-hajj <sam@aceaicompany.com>
2022-03-20 13:20:15 +0000

    ci: Configure deployment pipeline in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -131,3 +131,4 @@
 2022-03-16T13:47:33 - SUSTAINED_PEAK - refactor: Optimize recommendation in auth.py
 2022-03-17T13:03:26 - SUSTAINED_PEAK - ci: Configure deployment pipeline in auth.py
 2022-03-19T12:44:14 - SUSTAINED_PEAK - feat!: Major feature integration: search affecting api
+2022-03-20T13:20:15 - SUSTAINED_PEAK - ci: Configure deployment pipeline in auth.py
