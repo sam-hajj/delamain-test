2f66599ffa8b8af5cef6204b2211d92bc65eedbc
sam-hajj <sam@aceaicompany.com>
2022-03-24 15:00:52 +0000

    fix: Resolve bug #977 in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -168,3 +168,4 @@
 2022-03-21T13:07:14 - SUSTAINED_PEAK - perf: Improve query performance in test_endpoints.py
 2022-03-21T13:32:22 - SUSTAINED_PEAK - feat: Implement login endpoint in test_endpoints.py
 2022-03-23T10:20:28 - SUSTAINED_PEAK - feat: Implement profile endpoint in test_endpoints.py
+2022-03-24T15:00:52 - SUSTAINED_PEAK - fix: Resolve bug #977 in test_endpoints.py
