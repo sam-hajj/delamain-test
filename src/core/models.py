2cfc209d9cb91c438c6762f7790609c21ccfc969
sam-hajj <sam@aceaicompany.com>
2022-03-28 13:05:08 +0000

    fix!: Critical patch for vulnerability in models.py

--- src/core/models.py
+++ src/core/models.py
@@ -297,3 +297,4 @@
 2022-03-24T16:05:00 - SUSTAINED_PEAK - feat: Implement profile endpoint in models.py
 2022-03-24T16:51:19 - SUSTAINED_PEAK - perf: Improve query performance in models.py
 2022-03-27T13:06:40 - SUSTAINED_PEAK - fix: Resolve bug #235 in models.py
+2022-03-28T13:05:08 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in models.py
