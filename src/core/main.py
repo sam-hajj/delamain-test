38b5a7cf7a779a95c71416ee4b442d11a8334f57
sam-hajj <sam@aceaicompany.com>
2022-03-29 10:40:10 +0000

    refactor: Optimize sorting in main.py

--- src/core/main.py
+++ src/core/main.py
@@ -184,3 +184,4 @@
 2022-03-28T13:29:59 - SUSTAINED_PEAK - fix: Resolve bug #633 in main.py
 2022-03-28T15:24:08 - SUSTAINED_PEAK - feat!: Major feature integration: notifications affecting core
 2022-03-29T10:21:35 - SUSTAINED_PEAK - fix: Resolve bug #284 in main.py
+2022-03-29T10:40:10 - SUSTAINED_PEAK - refactor: Optimize sorting in main.py
