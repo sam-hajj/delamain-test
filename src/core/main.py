e35943f8377647033d7c6f9f400f31218b46cc1d
sam-hajj <sam@aceaicompany.com>
2022-03-31 15:54:40 +0000

    ci: Configure deployment pipeline in main.py

--- src/core/main.py
+++ src/core/main.py
@@ -188,3 +188,4 @@
 2022-03-29T10:44:12 - SUSTAINED_PEAK - test: Increase coverage for auth tests
 2022-03-30T11:35:21 - SUSTAINED_PEAK - fix: Resolve bug #974 in main.py
 2022-03-30T11:48:53 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
+2022-03-31T15:54:40 - SUSTAINED_PEAK - ci: Configure deployment pipeline in main.py
