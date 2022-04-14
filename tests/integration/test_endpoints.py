d5c2afb0717498f32407df1582c8aa21b95c5fd9
sam-hajj <sam@aceaicompany.com>
2022-04-14 15:47:45 +0000

    fix!: Critical patch for vulnerability in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -189,3 +189,4 @@
 2022-04-11T11:03:21 - SUSTAINED_PEAK - ci: Configure deployment pipeline in test_endpoints.py
 2022-04-14T12:19:07 - SUSTAINED_PEAK - feat: Implement payments endpoint in test_endpoints.py
 2022-04-14T13:09:05 - SUSTAINED_PEAK - fix: Resolve bug #680 in test_endpoints.py
+2022-04-14T15:47:45 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in test_endpoints.py
