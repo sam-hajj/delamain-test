9ca266b50bb328796e0255b404761449627b98b6
sam-hajj <sam@aceaicompany.com>
2022-04-14 12:19:07 +0000

    feat: Implement payments endpoint in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -187,3 +187,4 @@
 2022-04-09T12:45:50 - SUSTAINED_PEAK - fix: Resolve bug #912 in test_endpoints.py
 2022-04-10T11:39:16 - SUSTAINED_PEAK - fix: Resolve bug #683 in test_endpoints.py
 2022-04-11T11:03:21 - SUSTAINED_PEAK - ci: Configure deployment pipeline in test_endpoints.py
+2022-04-14T12:19:07 - SUSTAINED_PEAK - feat: Implement payments endpoint in test_endpoints.py
