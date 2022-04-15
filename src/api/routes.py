b7869a0a843582feb2d80ed19344eb3f45efddfc
sam-hajj <sam@aceaicompany.com>
2022-04-15 16:07:25 +0000

    fix!: Critical patch for vulnerability in routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -169,3 +169,4 @@
 2022-04-14T13:33:16 - SUSTAINED_PEAK - fix: Resolve bug #707 in routes.py
 2022-04-14T13:48:24 - SUSTAINED_PEAK - feat!: Major feature integration: data processing affecting api
 2022-04-15T14:13:33 - SUSTAINED_PEAK - perf: Improve query performance in routes.py
+2022-04-15T16:07:25 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in routes.py
