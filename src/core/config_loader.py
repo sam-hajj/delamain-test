fcaaade405161f2a9b41e56ce595e71f82ba5983
sam-hajj <sam@aceaicompany.com>
2022-04-27 13:17:47 +0000

    feat: POC for A/B testing framework in core

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -53,3 +53,4 @@
 2022-04-21T15:44:19 - PROJECT_SHIFT - refactor: Adapt user for new requirements in config_loader.py
 2022-04-24T12:35:32 - PROJECT_SHIFT - refactor: Modularize API gateway in config_loader.py
 2022-04-25T12:07:20 - PROJECT_SHIFT - docs: Update architecture diagram
+2022-04-27T13:17:47 - PROJECT_SHIFT - feat: POC for A/B testing framework in core
