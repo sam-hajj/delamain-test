052f459dc7d9bdc833c381090d422d2290dea0a0
sam-hajj <sam@aceaicompany.com>
2022-04-27 13:22:56 +0000

    test: Add integration tests for analytics dashboard

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -55,3 +55,4 @@
 2022-04-25T11:38:45 - PROJECT_SHIFT - docs: Update architecture diagram
 2022-04-26T12:43:35 - PROJECT_SHIFT - docs: Update architecture diagram
 2022-04-26T14:03:47 - PROJECT_SHIFT - refactor: Adapt order for new requirements in schemas.py
+2022-04-27T13:22:56 - PROJECT_SHIFT - test: Add integration tests for analytics dashboard
