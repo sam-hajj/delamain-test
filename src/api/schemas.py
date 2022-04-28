47f76a9f6133b636f6298ec18bb71484198f38b0
sam-hajj <sam@aceaicompany.com>
2022-04-28 10:27:18 +0000

    refactor: Modularize backend in schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -57,3 +57,4 @@
 2022-04-26T14:03:47 - PROJECT_SHIFT - refactor: Adapt order for new requirements in schemas.py
 2022-04-27T13:22:56 - PROJECT_SHIFT - test: Add integration tests for analytics dashboard
 2022-04-28T09:46:29 - PROJECT_SHIFT - refactor: Modularize database in schemas.py
+2022-04-28T10:27:18 - PROJECT_SHIFT - refactor: Modularize backend in schemas.py
