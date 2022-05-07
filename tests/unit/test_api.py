104ae1c2b1465265f6ca8013fce359b7b728390b
sam-hajj <sam@aceaicompany.com>
2022-05-07 12:06:33 +0000

    test: Add integration tests for A/B testing framework

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -65,3 +65,4 @@
 2022-05-06T14:23:37 - PROJECT_SHIFT - refactor: Modularize API gateway in test_api.py
 2022-05-06T14:39:32 - PROJECT_SHIFT - feat: POC for real-time updates in unit
 2022-05-06T14:52:52 - PROJECT_SHIFT - feat: POC for analytics dashboard in unit
+2022-05-07T12:06:33 - PROJECT_SHIFT - test: Add integration tests for A/B testing framework
