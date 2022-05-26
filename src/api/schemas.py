907a87cf096b34a818f419b7df623f064d5d533c
sam-hajj <sam@aceaicompany.com>
2022-05-26 15:01:47 +0000

    refactor: Adapt user for new requirements in schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -83,3 +83,4 @@
 2022-05-22T12:19:48 - PROJECT_SHIFT - test: Add integration tests for machine learning model
 2022-05-23T10:34:21 - PROJECT_SHIFT - docs: Update architecture diagram
 2022-05-25T11:40:56 - PROJECT_SHIFT - refactor: Adapt product for new requirements in schemas.py
+2022-05-26T15:01:47 - PROJECT_SHIFT - refactor: Adapt user for new requirements in schemas.py
