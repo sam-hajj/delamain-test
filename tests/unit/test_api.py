b08128b87f242c5297335db7bab9b8da5bae5e76
sam-hajj <sam@aceaicompany.com>
2022-05-30 16:04:34 +0000

    docs: Update architecture diagram

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -85,3 +85,4 @@
 2022-05-27T11:09:14 - PROJECT_SHIFT - refactor: Adapt reporting for new requirements in test_api.py
 2022-05-29T14:39:39 - PROJECT_SHIFT - refactor: Modularize database in test_api.py
 2022-05-29T14:52:50 - PROJECT_SHIFT - refactor: Modularize database in test_api.py
+2022-05-30T16:04:34 - PROJECT_SHIFT - docs: Update architecture diagram
