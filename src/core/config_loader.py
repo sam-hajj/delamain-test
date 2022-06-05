1cbe6344186baf2edde3fae37970dc5028600f12
sam-hajj <sam@aceaicompany.com>
2022-06-05 09:25:35 +0000

    refactor: Adapt order for new requirements in config_loader.py

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -93,3 +93,4 @@
 2022-06-01T13:59:04 - PROJECT_SHIFT - chore: Experiment with vue in config_loader.py
 2022-06-02T10:09:48 - PROJECT_SHIFT - docs: Update architecture diagram
 2022-06-03T15:15:15 - PROJECT_SHIFT - feat: POC for analytics dashboard in core
+2022-06-05T09:25:35 - PROJECT_SHIFT - refactor: Adapt order for new requirements in config_loader.py
