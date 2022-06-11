c931c25e19f62541f86f7adcea1b65ba46732b86
sam-hajj <sam@aceaicompany.com>
2022-06-11 12:53:52 +0000

    test: Add integration tests for machine learning model

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -97,3 +97,4 @@
 2022-06-07T11:46:34 - PROJECT_SHIFT - refactor: Modularize messaging queue in test_api.py
 2022-06-10T11:50:09 - PROJECT_SHIFT - refactor: Modularize messaging queue in test_api.py
 2022-06-10T12:02:34 - PROJECT_SHIFT - refactor: Modularize messaging queue in test_api.py
+2022-06-11T12:53:52 - PROJECT_SHIFT - test: Add integration tests for machine learning model
