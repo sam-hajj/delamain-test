8052fc6aecdc72d251b77ef7661cffd697609564
sam-hajj <sam@aceaicompany.com>
2022-07-05 12:00:00 +0000

    docs: Update architecture diagram

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -122,3 +122,4 @@
 2022-07-03T11:10:40 - PROJECT_SHIFT - feat: POC for A/B testing framework in api
 2022-07-03T11:38:06 - PROJECT_SHIFT - docs: Update architecture diagram
 2022-07-04T11:55:21 - PROJECT_SHIFT - feat: POC for A/B testing framework in api
+2022-07-05T12:00:00 - PROJECT_SHIFT - docs: Update architecture diagram
