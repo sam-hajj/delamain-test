2477af4a5a39789aa2101d1551660423a9bd3281
sam-hajj <sam@aceaicompany.com>
2022-07-14 11:19:49 +0000

    refactor: Adapt user for new requirements in schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -128,3 +128,4 @@
 2022-07-07T08:20:02 - PROJECT_SHIFT - feat: POC for analytics dashboard in api
 2022-07-08T15:21:25 - PROJECT_SHIFT - test: Add integration tests for machine learning model
 2022-07-13T10:50:38 - PROJECT_SHIFT - docs: Update architecture diagram
+2022-07-14T11:19:49 - PROJECT_SHIFT - refactor: Adapt user for new requirements in schemas.py
