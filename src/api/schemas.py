8a902969990b2eafc4e98fb2a8ff923795974d04
sam-hajj <sam@aceaicompany.com>
2022-07-16 13:14:56 +0000

    refactor: Adapt product for new requirements in schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -131,3 +131,4 @@
 2022-07-14T11:19:49 - PROJECT_SHIFT - refactor: Adapt user for new requirements in schemas.py
 2022-07-15T11:37:12 - PROJECT_SHIFT - test: Add integration tests for analytics dashboard
 2022-07-16T13:02:17 - PROJECT_SHIFT - chore: Experiment with vue in schemas.py
+2022-07-16T13:14:56 - PROJECT_SHIFT - refactor: Adapt product for new requirements in schemas.py
