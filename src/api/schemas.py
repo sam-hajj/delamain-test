1e81efa00b0988ff6199e77e8e3f60b6667fb98c
sam-hajj <sam@aceaicompany.com>
2022-07-17 14:17:16 +0000

    docs: Update architecture diagram

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -134,3 +134,4 @@
 2022-07-16T13:14:56 - PROJECT_SHIFT - refactor: Adapt product for new requirements in schemas.py
 2022-07-16T13:39:54 - PROJECT_SHIFT - chore: Experiment with vue in schemas.py
 2022-07-17T14:03:20 - PROJECT_SHIFT - refactor: Modularize API gateway in schemas.py
+2022-07-17T14:17:16 - PROJECT_SHIFT - docs: Update architecture diagram
