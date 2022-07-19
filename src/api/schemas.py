5085a1eb640900e37ee1c92ea4179073a0a40634
sam-hajj <sam@aceaicompany.com>
2022-07-19 11:08:49 +0000

    feat: POC for real-time updates in api

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -137,3 +137,4 @@
 2022-07-17T14:17:16 - PROJECT_SHIFT - docs: Update architecture diagram
 2022-07-17T14:24:39 - PROJECT_SHIFT - refactor: Modularize messaging queue in schemas.py
 2022-07-17T14:48:45 - PROJECT_SHIFT - test: Add integration tests for A/B testing framework
+2022-07-19T11:08:49 - PROJECT_SHIFT - feat: POC for real-time updates in api
