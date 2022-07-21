967bfbbd355e44c16bf9ab4b33b9ca5373bcd051
sam-hajj <sam@aceaicompany.com>
2022-07-21 12:23:17 +0000

    refactor: Modularize database in schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -140,3 +140,4 @@
 2022-07-19T11:08:49 - PROJECT_SHIFT - feat: POC for real-time updates in api
 2022-07-20T15:12:46 - PROJECT_SHIFT - refactor: Adapt reporting for new requirements in schemas.py
 2022-07-21T12:18:40 - PROJECT_SHIFT - chore: Experiment with django in schemas.py
+2022-07-21T12:23:17 - PROJECT_SHIFT - refactor: Modularize database in schemas.py
