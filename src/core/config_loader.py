24a63234a1fe772b8c846882d4761661b0eb9f92
sam-hajj <sam@aceaicompany.com>
2022-07-26 11:56:15 +0000

    test: Add integration tests for A/B testing framework

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -138,3 +138,4 @@
 2022-07-20T14:41:24 - PROJECT_SHIFT - docs: Update architecture diagram
 2022-07-20T14:42:29 - PROJECT_SHIFT - test: Add integration tests for machine learning model
 2022-07-23T14:23:43 - PROJECT_SHIFT - test: Add integration tests for real-time updates
+2022-07-26T11:56:15 - PROJECT_SHIFT - test: Add integration tests for A/B testing framework
