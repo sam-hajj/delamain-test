b9dc06d5e820f811393aa4067deeb10d1f5318c6
sam-hajj <sam@aceaicompany.com>
2022-07-29 13:02:06 +0000

    refactor: Adapt product for new requirements in schemas.py

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -147,3 +147,4 @@
 2022-07-25T13:45:42 - PROJECT_SHIFT - docs: Update architecture diagram
 2022-07-26T11:54:24 - PROJECT_SHIFT - refactor: Adapt product for new requirements in schemas.py
 2022-07-29T13:00:41 - PROJECT_SHIFT - refactor: Adapt reporting for new requirements in schemas.py
+2022-07-29T13:02:06 - PROJECT_SHIFT - refactor: Adapt product for new requirements in schemas.py
