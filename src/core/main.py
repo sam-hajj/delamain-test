f7d9fcf35f4870d36d7d42a4ac19e5f1bee04fc6
sam-hajj <sam@aceaicompany.com>
2022-08-08 13:05:24 +0000

    perf: Improve query performance in main.py

--- src/core/main.py
+++ src/core/main.py
@@ -201,3 +201,4 @@
 2022-04-20T11:32:01 - SUSTAINED_PEAK - ci: Configure deployment pipeline in main.py
 2022-04-20T12:29:43 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
 2022-08-08T12:02:23 - SUSTAINED_PEAK - feat: Implement notifications endpoint in main.py
+2022-08-08T13:05:24 - SUSTAINED_PEAK - perf: Improve query performance in main.py
