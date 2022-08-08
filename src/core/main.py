03918fc5b6fc4cc5f4150f6563a012e18e1391c1
sam-hajj <sam@aceaicompany.com>
2022-08-08 12:02:23 +0000

    feat: Implement notifications endpoint in main.py

--- src/core/main.py
+++ src/core/main.py
@@ -200,3 +200,4 @@
 2022-04-20T10:34:51 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in main.py
 2022-04-20T11:32:01 - SUSTAINED_PEAK - ci: Configure deployment pipeline in main.py
 2022-04-20T12:29:43 - SUSTAINED_PEAK - test: Increase coverage for inventory tests
+2022-08-08T12:02:23 - SUSTAINED_PEAK - feat: Implement notifications endpoint in main.py
