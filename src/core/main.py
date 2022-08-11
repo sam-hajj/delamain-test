970224734d10fd2ae500e61b4db164668cbe6dd6
sam-hajj <sam@aceaicompany.com>
2022-08-11 15:37:02 +0000

    perf: Improve query performance in main.py

--- src/core/main.py
+++ src/core/main.py
@@ -204,3 +204,4 @@
 2022-08-08T13:05:24 - SUSTAINED_PEAK - perf: Improve query performance in main.py
 2022-08-08T14:55:44 - SUSTAINED_PEAK - perf: Improve query performance in main.py
 2022-08-09T14:58:02 - SUSTAINED_PEAK - feat: Implement search endpoint in main.py
+2022-08-11T15:37:02 - SUSTAINED_PEAK - perf: Improve query performance in main.py
