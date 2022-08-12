aaf4f3dd85d4fa5951f34fae6e2e557dc064c833
sam-hajj <sam@aceaicompany.com>
2022-08-12 13:17:17 +0000

    test: Increase coverage for reporting tests

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -199,3 +199,4 @@
 2022-08-11T15:21:21 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in test_endpoints.py
 2022-08-11T15:35:03 - SUSTAINED_PEAK - fix: Resolve bug #474 in test_endpoints.py
 2022-08-12T11:25:46 - SUSTAINED_PEAK - feat!: Major feature integration: payments affecting integration
+2022-08-12T13:17:17 - SUSTAINED_PEAK - test: Increase coverage for reporting tests
