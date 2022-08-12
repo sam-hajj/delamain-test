2e969c76fba3ecb542593c6527f8439263fecf24
sam-hajj <sam@aceaicompany.com>
2022-08-12 12:53:48 +0000

    fix!: Critical patch for vulnerability in routes.py

--- src/api/routes.py
+++ src/api/routes.py
@@ -174,3 +174,4 @@
 2022-08-11T15:09:37 - SUSTAINED_PEAK - perf: Improve query performance in routes.py
 2022-08-11T15:33:26 - SUSTAINED_PEAK - ci: Configure deployment pipeline in routes.py
 2022-08-11T15:37:35 - SUSTAINED_PEAK - refactor: Optimize recommendation in routes.py
+2022-08-12T12:53:48 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in routes.py
