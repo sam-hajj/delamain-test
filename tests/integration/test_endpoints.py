5caa50b05ced674a17217d119325874edd355998
sam-hajj <sam@aceaicompany.com>
2022-08-17 15:36:54 +0000

    test: Increase coverage for user tests

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -208,3 +208,4 @@
 2022-08-15T16:14:58 - SUSTAINED_PEAK - fix: Resolve bug #593 in test_endpoints.py
 2022-08-16T12:24:57 - SUSTAINED_PEAK - refactor: Optimize sorting in test_endpoints.py
 2022-08-17T15:14:25 - SUSTAINED_PEAK - refactor: Optimize recommendation in test_endpoints.py
+2022-08-17T15:36:54 - SUSTAINED_PEAK - test: Increase coverage for user tests
