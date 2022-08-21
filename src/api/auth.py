47944dbc1624ad5208aa93d2347467dc860c4a72
sam-hajj <sam@aceaicompany.com>
2022-08-21 13:12:28 +0000

    refactor: Optimize hashing in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -184,3 +184,4 @@
 2022-08-20T13:22:16 - SUSTAINED_PEAK - refactor: Optimize sorting in auth.py
 2022-08-20T13:58:55 - SUSTAINED_PEAK - ci: Configure deployment pipeline in auth.py
 2022-08-21T13:11:01 - SUSTAINED_PEAK - fix: Resolve bug #247 in auth.py
+2022-08-21T13:12:28 - SUSTAINED_PEAK - refactor: Optimize hashing in auth.py
