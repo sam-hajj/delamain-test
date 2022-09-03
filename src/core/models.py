db19ad1691503930381844490f851c4706b9fb6b
sam-hajj <sam@aceaicompany.com>
2022-09-03 11:01:55 +0000

    test: Increase coverage for reporting tests

--- src/core/models.py
+++ src/core/models.py
@@ -339,3 +339,4 @@
 2022-08-25T13:47:14 - SUSTAINED_PEAK - fix: Resolve bug #627 in models.py
 2022-08-26T15:00:49 - SUSTAINED_PEAK - feat: Implement login endpoint in models.py
 2022-08-30T11:35:35 - SUSTAINED_PEAK - perf: Improve query performance in models.py
+2022-09-03T11:01:55 - SUSTAINED_PEAK - test: Increase coverage for reporting tests
