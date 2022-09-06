bbea8afccae88f82ee73ef09f606c52a52fe3221
sam-hajj <sam@aceaicompany.com>
2022-09-06 09:04:04 +0000

    ci: Configure deployment pipeline in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -227,3 +227,4 @@
 2022-09-05T13:00:28 - SUSTAINED_PEAK - perf: Improve query performance in test_endpoints.py
 2022-09-05T15:43:05 - SUSTAINED_PEAK - refactor: Optimize pathfinding in test_endpoints.py
 2022-09-05T16:22:02 - SUSTAINED_PEAK - refactor: Optimize sorting in test_endpoints.py
+2022-09-06T09:04:04 - SUSTAINED_PEAK - ci: Configure deployment pipeline in test_endpoints.py
