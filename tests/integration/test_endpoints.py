09f7b6361a9cf1a54f5614024978530f9a0e7f02
sam-hajj <sam@aceaicompany.com>
2022-09-13 13:21:17 +0000

    ci: Configure deployment pipeline in test_endpoints.py

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -242,3 +242,4 @@
 2022-09-10T12:01:08 - SUSTAINED_PEAK - refactor: Optimize sorting in test_endpoints.py
 2022-09-10T12:09:06 - SUSTAINED_PEAK - ci: Configure deployment pipeline in test_endpoints.py
 2022-09-12T12:38:35 - SUSTAINED_PEAK - feat: Implement login endpoint in test_endpoints.py
+2022-09-13T13:21:17 - SUSTAINED_PEAK - ci: Configure deployment pipeline in test_endpoints.py
