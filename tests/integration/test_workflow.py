72c3b0f566150f9863b3ac12b851f70dbe80597f
sam-hajj <sam@aceaicompany.com>
2022-10-09 09:33:33 +0000

    style: Formatting adjustments in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -24,3 +24,4 @@
 2022-10-05T12:48:15 - MAINTENANCE_LOW - chore: Update dependencies (maven)
 2022-10-05T12:50:55 - MAINTENANCE_LOW - chore: Update dependencies (gradle)
 2022-10-06T11:44:54 - MAINTENANCE_LOW - ci: Tweak workflow triggers in test_workflow.py
+2022-10-09T09:33:33 - MAINTENANCE_LOW - style: Formatting adjustments in test_workflow.py
