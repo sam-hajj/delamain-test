00154be36629f62d3b86009c67c72dc4e5a516a3
sam-hajj <sam@aceaicompany.com>
2022-10-17 10:44:03 +0000

    chore: Update dependencies (npm)

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -35,3 +35,4 @@
 2022-10-16T13:08:06 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
 2022-10-16T13:33:01 - MAINTENANCE_LOW - ci: Tweak workflow triggers in test_workflow.py
 2022-10-16T13:51:37 - MAINTENANCE_LOW - chore: Update dependencies (pip)
+2022-10-17T10:44:03 - MAINTENANCE_LOW - chore: Update dependencies (npm)
