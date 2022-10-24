3a9eb282296765db947b909c96359b25969269cc
sam-hajj <sam@aceaicompany.com>
2022-10-24 10:51:49 +0000

    fix: Small bugfix in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -45,3 +45,4 @@
 2022-10-22T10:52:08 - MAINTENANCE_LOW - chore: Rotate logs or cleanup artifacts
 2022-10-24T10:00:00 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
 2022-10-24T10:25:13 - MAINTENANCE_LOW - chore: Update dependencies (gradle)
+2022-10-24T10:51:49 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
