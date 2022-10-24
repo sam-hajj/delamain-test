1e4ad5219c4530b6a4d1b74f3582a206e5e27d93
sam-hajj <sam@aceaicompany.com>
2022-10-24 10:00:00 +0000

    docs: Minor clarification in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -43,3 +43,4 @@
 2022-10-21T11:55:00 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
 2022-10-22T10:50:46 - MAINTENANCE_LOW - ci: Tweak workflow triggers in test_workflow.py
 2022-10-22T10:52:08 - MAINTENANCE_LOW - chore: Rotate logs or cleanup artifacts
+2022-10-24T10:00:00 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
