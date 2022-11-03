adda1609e230dfbb8b7f57d6c367484f58b65162
sam-hajj <sam@aceaicompany.com>
2022-11-03 12:51:08 +0000

    ci: Tweak workflow triggers in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -58,3 +58,4 @@
 2022-11-01T14:52:48 - MAINTENANCE_LOW - chore: Update dependencies (yarn)
 2022-11-02T14:44:18 - MAINTENANCE_LOW - chore: Rotate logs or cleanup artifacts
 2022-11-02T15:20:26 - MAINTENANCE_LOW - style: Formatting adjustments in test_workflow.py
+2022-11-03T12:51:08 - MAINTENANCE_LOW - ci: Tweak workflow triggers in test_workflow.py
