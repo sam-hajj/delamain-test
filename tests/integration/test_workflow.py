bd6c64bcb623d14c739802f3134a12ccf8b9597c
sam-hajj <sam@aceaicompany.com>
2022-12-02 12:19:19 +0000

    chore: Rotate logs or cleanup artifacts

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -110,3 +110,4 @@
 2022-12-01T14:07:40 - MAINTENANCE_LOW - style: Formatting adjustments in test_workflow.py
 2022-12-01T14:18:04 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
 2022-12-02T12:10:01 - MAINTENANCE_LOW - chore: Update dependencies (yarn)
+2022-12-02T12:19:19 - MAINTENANCE_LOW - chore: Rotate logs or cleanup artifacts
