5905b1cac421fb557c03550693a1b6fdc5ebe1ca
sam-hajj <sam@aceaicompany.com>
2022-12-07 10:25:37 +0000

    ci: Tweak workflow triggers in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -117,3 +117,4 @@
 2022-12-03T10:39:54 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
 2022-12-03T10:45:52 - MAINTENANCE_LOW - ci: Tweak workflow triggers in test_workflow.py
 2022-12-06T13:23:34 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
+2022-12-07T10:25:37 - MAINTENANCE_LOW - ci: Tweak workflow triggers in test_workflow.py
