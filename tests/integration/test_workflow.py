108812c509e4cebb4d9b9e730c420e84a154ea5f
sam-hajj <sam@aceaicompany.com>
2023-02-28 10:41:55 +0000

    chore: Update dependencies (maven)

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -226,3 +226,4 @@
 2023-02-27T12:59:32 - MAINTENANCE_LOW - style: Formatting adjustments in test_workflow.py
 2023-02-28T09:14:53 - MAINTENANCE_LOW - chore: Update dependencies (yarn)
 2023-02-28T10:09:16 - MAINTENANCE_LOW - chore: Rotate logs or cleanup artifacts
+2023-02-28T10:41:55 - MAINTENANCE_LOW - chore: Update dependencies (maven)
