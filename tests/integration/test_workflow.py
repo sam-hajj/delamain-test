89e540b1bf262507a42c55234808821ad0257ac0
sam-hajj <sam@aceaicompany.com>
2023-03-09 15:36:05 +0000

    style: Formatting adjustments in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -243,3 +243,4 @@
 2023-03-08T12:05:19 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
 2023-03-08T13:02:31 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
 2023-03-09T15:18:49 - MAINTENANCE_LOW - style: Formatting adjustments in test_workflow.py
+2023-03-09T15:36:05 - MAINTENANCE_LOW - style: Formatting adjustments in test_workflow.py
