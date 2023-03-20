a1bcf6a3d00c27ba6bf3ef5789540cd753693ab0
sam-hajj <sam@aceaicompany.com>
2023-03-20 17:32:29 +0000

    test: Add initial unit tests for utils.py

--- src/core/utils.py
+++ src/core/utils.py
@@ -189,3 +189,4 @@
 2023-03-17T12:10:47 - RAMPING_UP - refactor: Improve auth structure in core
 2023-03-18T13:06:26 - RAMPING_UP - refactor: Improve inventory structure in core
 2023-03-20T17:05:17 - RAMPING_UP - refactor: Improve user structure in core
+2023-03-20T17:32:29 - RAMPING_UP - test: Add initial unit tests for utils.py
