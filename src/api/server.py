460a8cc55a9442182936c0bb1e31f3d3f13b528c
sam-hajj <sam@aceaicompany.com>
2023-04-04 12:55:24 +0000

    docs: Detail setup instructions

--- src/api/server.py
+++ src/api/server.py
@@ -125,3 +125,4 @@
 2023-04-01T09:52:28 - RAMPING_UP - docs: Detail setup instructions
 2023-04-01T10:37:14 - RAMPING_UP - refactor: Improve reporting structure in api
 2023-04-02T10:03:12 - RAMPING_UP - feat: Add core logic for caching service in server.py
+2023-04-04T12:55:24 - RAMPING_UP - docs: Detail setup instructions
