e85d460a9a7c90217d396f0424dbf479102cc136
sam-hajj <sam@aceaicompany.com>
2023-04-17 14:08:08 +0000

    refactor: Improve reporting structure in api

--- src/api/server.py
+++ src/api/server.py
@@ -133,3 +133,4 @@
 2023-04-12T12:08:13 - RAMPING_UP - feat: Add core logic for caching service in server.py
 2023-04-12T12:38:13 - RAMPING_UP - feat: Add core logic for login service in server.py
 2023-04-17T13:49:28 - RAMPING_UP - chore: Update requirements.txt
+2023-04-17T14:08:08 - RAMPING_UP - refactor: Improve reporting structure in api
