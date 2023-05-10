8bdf6f9fe1e4d0fc8bff42cee24105e138069cd7
sam-hajj <sam@aceaicompany.com>
2023-05-10 16:02:17 +0000

    test: Increase coverage for product tests

--- tests/integration/test_endpoints.py
+++ tests/integration/test_endpoints.py
@@ -249,3 +249,4 @@
 2022-09-14T13:13:51 - SUSTAINED_PEAK - perf: Improve query performance in test_endpoints.py
 2022-09-15T11:09:01 - SUSTAINED_PEAK - test: Increase coverage for user tests
 2022-09-15T11:51:54 - SUSTAINED_PEAK - feat: Implement caching endpoint in test_endpoints.py
+2023-05-10T16:02:17 - SUSTAINED_PEAK - test: Increase coverage for product tests
