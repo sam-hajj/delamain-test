3a9e4026f74ded92394f0da8b6f2111fae8c4a10
sam-hajj <sam@aceaicompany.com>
2023-05-12 08:21:28 +0000

    fix!: Critical patch for vulnerability in auth.py

--- src/api/auth.py
+++ src/api/auth.py
@@ -211,3 +211,4 @@
 2022-09-12T14:40:53 - SUSTAINED_PEAK - refactor: Optimize recommendation in auth.py
 2022-09-15T09:17:17 - SUSTAINED_PEAK - refactor: Optimize recommendation in auth.py
 2023-05-10T16:12:37 - SUSTAINED_PEAK - feat!: Major feature integration: payments affecting api
+2023-05-12T08:21:28 - SUSTAINED_PEAK - fix!: Critical patch for vulnerability in auth.py
