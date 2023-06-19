fd70e4a2e7313d62cc0da373a992e5f3fbcb8b3a
sam-hajj <sam@aceaicompany.com>
2023-06-19 10:10:08 +0000

    chore: Rotate logs or cleanup artifacts

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -278,3 +278,4 @@
 2023-06-14T14:37:27 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
 2023-06-16T13:51:18 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
 2023-06-18T12:52:28 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
+2023-06-19T10:10:08 - MAINTENANCE_LOW - chore: Rotate logs or cleanup artifacts
