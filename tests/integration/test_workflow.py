37499dd791b51d7ed3471cc72360020c2468ad2c
sam-hajj <sam@aceaicompany.com>
2023-06-26 15:55:05 +0000

    fix: Small bugfix in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -291,3 +291,4 @@
 2023-06-23T11:12:32 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
 2023-06-23T11:52:03 - MAINTENANCE_LOW - chore: Update dependencies (pip)
 2023-06-25T14:45:23 - MAINTENANCE_LOW - chore: Rotate logs or cleanup artifacts
+2023-06-26T15:55:05 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
