5faceb2612a9b6e6bc429e220896e18cff6a0eaf
sam-hajj <sam@aceaicompany.com>
2023-07-30 12:47:12 +0000

    ci: Tweak workflow triggers in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -331,3 +331,4 @@
 2023-07-28T13:03:00 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
 2023-07-30T12:23:22 - MAINTENANCE_LOW - ci: Tweak workflow triggers in test_workflow.py
 2023-07-30T12:34:12 - MAINTENANCE_LOW - chore: Rotate logs or cleanup artifacts
+2023-07-30T12:47:12 - MAINTENANCE_LOW - ci: Tweak workflow triggers in test_workflow.py
