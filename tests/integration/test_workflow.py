df3b60420c22f818fbb2b3edec5d7644ecb89131
sam-hajj <sam@aceaicompany.com>
2023-08-10 12:17:05 +0000

    style: Formatting adjustments in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -365,3 +365,4 @@
 2023-08-09T13:01:19 - MAINTENANCE_LOW - chore: Rotate logs or cleanup artifacts
 2023-08-09T13:56:52 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
 2023-08-10T11:06:35 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
+2023-08-10T12:17:05 - MAINTENANCE_LOW - style: Formatting adjustments in test_workflow.py
