137ef12e48694a217e41affdaa827c7460175ef2
sam-hajj <sam@aceaicompany.com>
2023-10-20 10:00:35 +0000

    docs: Update architecture diagram

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -149,3 +149,4 @@
 2022-08-05T14:44:43 - PROJECT_SHIFT - docs: Update architecture diagram
 2023-10-20T09:05:52 - PROJECT_SHIFT - refactor: Adapt user for new requirements in config_loader.py
 2023-10-20T09:49:53 - PROJECT_SHIFT - chore: Experiment with requests in config_loader.py
+2023-10-20T10:00:35 - PROJECT_SHIFT - docs: Update architecture diagram
