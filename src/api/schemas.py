1d8e282069aa31f54adc235391e2936048a583d6
sam-hajj <sam@aceaicompany.com>
2023-10-21 15:58:44 +0000

    test: Add integration tests for analytics dashboard

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -159,3 +159,4 @@
 2022-08-05T14:58:41 - PROJECT_SHIFT - test: Add integration tests for machine learning model
 2023-10-19T13:24:31 - PROJECT_SHIFT - test: Add integration tests for machine learning model
 2023-10-19T13:37:46 - PROJECT_SHIFT - test: Add integration tests for A/B testing framework
+2023-10-21T15:58:44 - PROJECT_SHIFT - test: Add integration tests for analytics dashboard
