a854b866622a56059f196868065c599c79b2f7a8
sam-hajj <sam@aceaicompany.com>
2023-10-27 12:37:21 +0000

    feat: POC for machine learning model in api

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -166,3 +166,4 @@
 2023-10-23T10:22:55 - PROJECT_SHIFT - test: Add integration tests for A/B testing framework
 2023-10-23T11:28:45 - PROJECT_SHIFT - chore: Experiment with django in schemas.py
 2023-10-23T12:05:57 - PROJECT_SHIFT - refactor: Adapt auth for new requirements in schemas.py
+2023-10-27T12:37:21 - PROJECT_SHIFT - feat: POC for machine learning model in api
