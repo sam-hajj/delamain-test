8842480180707dfd0f1a3fc67197657a17e03629
sam-hajj <sam@aceaicompany.com>
2023-11-06 14:40:25 +0000

    test: Add integration tests for real-time updates

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -169,3 +169,4 @@
 2023-11-05T13:15:35 - PROJECT_SHIFT - chore: Experiment with vue in config_loader.py
 2023-11-06T13:29:19 - PROJECT_SHIFT - refactor: Adapt auth for new requirements in config_loader.py
 2023-11-06T13:51:46 - PROJECT_SHIFT - feat: POC for machine learning model in core
+2023-11-06T14:40:25 - PROJECT_SHIFT - test: Add integration tests for real-time updates
