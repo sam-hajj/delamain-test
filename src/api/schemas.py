1e6f0c8adfac94304d38fcc8be191da6b6bf4799
sam-hajj <sam@aceaicompany.com>
2023-11-08 10:49:50 +0000

    test: Add integration tests for real-time updates

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -173,3 +173,4 @@
 2023-11-03T13:39:54 - PROJECT_SHIFT - test: Add integration tests for machine learning model
 2023-11-08T09:59:51 - PROJECT_SHIFT - docs: Update architecture diagram
 2023-11-08T10:26:16 - PROJECT_SHIFT - refactor: Adapt order for new requirements in schemas.py
+2023-11-08T10:49:50 - PROJECT_SHIFT - test: Add integration tests for real-time updates
