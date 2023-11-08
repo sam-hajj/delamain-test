c8bd03ab54d4ff04d64b7bd18dc9bca2090fb8c3
sam-hajj <sam@aceaicompany.com>
2023-11-08 10:44:43 +0000

    test: Add integration tests for real-time updates

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -171,3 +171,4 @@
 2023-11-06T13:51:46 - PROJECT_SHIFT - feat: POC for machine learning model in core
 2023-11-06T14:40:25 - PROJECT_SHIFT - test: Add integration tests for real-time updates
 2023-11-08T09:07:31 - PROJECT_SHIFT - docs: Update architecture diagram
+2023-11-08T10:44:43 - PROJECT_SHIFT - test: Add integration tests for real-time updates
