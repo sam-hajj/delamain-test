354010daeb67d9ed62d3735a9b2cf73abe68eea5
sam-hajj <sam@aceaicompany.com>
2023-11-25 15:30:32 +0000

    refactor: Modularize messaging queue in config_loader.py

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -189,3 +189,4 @@
 2023-11-23T11:02:03 - PROJECT_SHIFT - chore: Experiment with fastapi in config_loader.py
 2023-11-23T11:29:57 - PROJECT_SHIFT - test: Add integration tests for machine learning model
 2023-11-25T15:05:54 - PROJECT_SHIFT - feat: POC for analytics dashboard in core
+2023-11-25T15:30:32 - PROJECT_SHIFT - refactor: Modularize messaging queue in config_loader.py
