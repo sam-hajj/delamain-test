6dad6f6762d5ff1dc638479fa248063eee10d805
sam-hajj <sam@aceaicompany.com>
2023-12-06 18:17:16 +0000

    chore: Experiment with pandas in test_api.py

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -189,3 +189,4 @@
 2023-12-01T15:19:55 - PROJECT_SHIFT - feat: POC for analytics dashboard in unit
 2023-12-01T15:28:12 - PROJECT_SHIFT - feat: POC for real-time updates in unit
 2023-12-04T15:49:46 - PROJECT_SHIFT - refactor: Modularize UI in test_api.py
+2023-12-06T18:17:16 - PROJECT_SHIFT - chore: Experiment with pandas in test_api.py
