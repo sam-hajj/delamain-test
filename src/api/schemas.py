ee320c0d65b8e463e6097da994075f98ccac39ea
sam-hajj <sam@aceaicompany.com>
2023-12-07 12:31:02 +0000

    test: Add integration tests for real-time updates

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -202,3 +202,4 @@
 2023-12-04T15:55:06 - PROJECT_SHIFT - feat: POC for real-time updates in api
 2023-12-05T13:57:12 - PROJECT_SHIFT - test: Add integration tests for analytics dashboard
 2023-12-06T16:30:09 - PROJECT_SHIFT - refactor: Modularize API gateway in schemas.py
+2023-12-07T12:31:02 - PROJECT_SHIFT - test: Add integration tests for real-time updates
