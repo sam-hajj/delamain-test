6b383283cc47fd83c7fa70bc84e202be9fbacddf
sam-hajj <sam@aceaicompany.com>
2023-12-08 13:34:34 +0000

    feat: POC for A/B testing framework in unit

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -196,3 +196,4 @@
 2023-12-07T12:35:20 - PROJECT_SHIFT - test: Add integration tests for real-time updates
 2023-12-08T13:09:53 - PROJECT_SHIFT - refactor: Modularize UI in test_api.py
 2023-12-08T13:30:51 - PROJECT_SHIFT - refactor: Adapt user for new requirements in test_api.py
+2023-12-08T13:34:34 - PROJECT_SHIFT - feat: POC for A/B testing framework in unit
