88a8bcfd14f0da529d32176f85f3638e050420f9
sam-hajj <sam@aceaicompany.com>
2023-12-18 13:33:49 +0000

    chore: Experiment with requests in config_loader.py

--- src/core/config_loader.py
+++ src/core/config_loader.py
@@ -219,3 +219,4 @@
 2023-12-16T13:32:56 - PROJECT_SHIFT - refactor: Modularize messaging queue in config_loader.py
 2023-12-17T14:03:39 - PROJECT_SHIFT - refactor: Adapt user for new requirements in config_loader.py
 2023-12-18T13:09:46 - PROJECT_SHIFT - chore: Experiment with fastapi in config_loader.py
+2023-12-18T13:33:49 - PROJECT_SHIFT - chore: Experiment with requests in config_loader.py
