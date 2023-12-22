3323e61e37c5cca22177cb55b09684ad3ddf442b
sam-hajj <sam@aceaicompany.com>
2023-12-22 12:10:12 +0000

    docs: Update architecture diagram

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -213,3 +213,4 @@
 2023-12-18T13:26:21 - PROJECT_SHIFT - test: Add integration tests for real-time updates
 2023-12-19T13:18:02 - PROJECT_SHIFT - docs: Update architecture diagram
 2023-12-20T13:28:00 - PROJECT_SHIFT - chore: Experiment with pandas in test_api.py
+2023-12-22T12:10:12 - PROJECT_SHIFT - docs: Update architecture diagram
