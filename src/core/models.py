a848e9d9a983c4060dd12cea5156d8e5635689c5
sam-hajj <sam@aceaicompany.com>
2024-01-09 13:52:35 +0000

    refactor: Improve inventory structure in core

--- src/core/models.py
+++ src/core/models.py
@@ -399,3 +399,4 @@
 2024-01-02T13:40:35 - RAMPING_UP - feat: Add core logic for payments service in models.py
 2024-01-07T11:17:52 - RAMPING_UP - docs: Detail setup instructions
 2024-01-08T14:18:13 - RAMPING_UP - feat: Implement basic profile in models.py
+2024-01-09T13:52:35 - RAMPING_UP - refactor: Improve inventory structure in core
