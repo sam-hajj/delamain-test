00acdffee2125f9591ff302bd2a34740d4dbd3c6
sam-hajj <sam@aceaicompany.com>
2024-01-12 12:45:29 +0000

    refactor: Improve user structure in unit

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -154,3 +154,4 @@
 2024-01-11T12:12:40 - RAMPING_UP - chore: Update requirements.txt
 2024-01-11T12:43:10 - RAMPING_UP - chore: Update requirements.txt
 2024-01-12T12:40:14 - RAMPING_UP - test: Add initial unit tests for test_core.py
+2024-01-12T12:45:29 - RAMPING_UP - refactor: Improve user structure in unit
