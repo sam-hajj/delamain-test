35bc64c8e7fd0decd7e30bacf05b11cd8c03d5c0
sam-hajj <sam@aceaicompany.com>
2024-01-30 15:31:36 +0000

    test: Add initial unit tests for server.py

--- src/api/server.py
+++ src/api/server.py
@@ -160,3 +160,4 @@
 2024-01-21T12:18:11 - RAMPING_UP - chore: Update requirements.txt
 2024-01-24T13:14:37 - RAMPING_UP - feat: Add core logic for search service in server.py
 2024-01-27T13:48:49 - RAMPING_UP - chore: Update requirements.txt
+2024-01-30T15:31:36 - RAMPING_UP - test: Add initial unit tests for server.py
