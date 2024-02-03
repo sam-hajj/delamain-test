5003f7d1decd86886ea34cef4ef4afe94979a621
sam-hajj <sam@aceaicompany.com>
2024-02-03 16:41:37 +0000

    refactor: Improve user structure in api

--- src/api/server.py
+++ src/api/server.py
@@ -166,3 +166,4 @@
 2024-02-01T11:46:21 - RAMPING_UP - chore: Update requirements.txt
 2024-02-01T11:58:39 - RAMPING_UP - docs: Detail setup instructions
 2024-02-02T15:32:30 - RAMPING_UP - chore: Update requirements.txt
+2024-02-03T16:41:37 - RAMPING_UP - refactor: Improve user structure in api
