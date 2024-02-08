2e92b102ffcf7d29a454cce8db7fc8f1d0fa8b29
sam-hajj <sam@aceaicompany.com>
2024-02-08 10:48:53 +0000

    docs: Detail setup instructions

--- src/core/models.py
+++ src/core/models.py
@@ -416,3 +416,4 @@
 2024-02-04T12:38:28 - RAMPING_UP - feat: Implement basic login in models.py
 2024-02-05T12:42:33 - RAMPING_UP - feat: Implement basic search in models.py
 2024-02-08T10:02:37 - RAMPING_UP - feat: Add core logic for profile service in models.py
+2024-02-08T10:48:53 - RAMPING_UP - docs: Detail setup instructions
