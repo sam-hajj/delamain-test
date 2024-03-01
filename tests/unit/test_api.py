c6ac73c1678584c35d828fe3760a25c19579e51a
sam-hajj <sam@aceaicompany.com>
2024-03-01 16:57:43 +0000

    refactor: Adapt user for new requirements in test_api.py

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -224,3 +224,4 @@
 2024-02-28T13:35:15 - PROJECT_SHIFT - refactor: Modularize UI in test_api.py
 2024-02-28T13:49:56 - PROJECT_SHIFT - test: Add integration tests for analytics dashboard
 2024-02-28T15:47:45 - PROJECT_SHIFT - refactor: Modularize UI in test_api.py
+2024-03-01T16:57:43 - PROJECT_SHIFT - refactor: Adapt user for new requirements in test_api.py
