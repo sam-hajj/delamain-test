e046f9d3031d4a69dc3357a6da072c2c2611ae50
sam-hajj <sam@aceaicompany.com>
2024-05-08 12:07:04 +0000

    chore: Rotate logs or cleanup artifacts

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -518,3 +518,4 @@
 2024-05-03T16:51:30 - MAINTENANCE_LOW - style: Formatting adjustments in test_workflow.py
 2024-05-05T10:09:12 - MAINTENANCE_LOW - chore: Update dependencies (gradle)
 2024-05-05T12:52:04 - MAINTENANCE_LOW - style: Formatting adjustments in test_workflow.py
+2024-05-08T12:07:04 - MAINTENANCE_LOW - chore: Rotate logs or cleanup artifacts
