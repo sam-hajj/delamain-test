fcb3de4daf1fba7a14ec9cda52fcdfee09ac0b98
sam-hajj <sam@aceaicompany.com>
2024-11-03 14:39:31 +0000

    docs: Add usage examples for profile

--- tests/unit/test_utils.py
+++ tests/unit/test_utils.py
@@ -80,3 +80,4 @@
 2024-10-25T13:19:51 - COOLING_DOWN - refactor: Code cleanup in test_utils.py
 2024-10-26T11:26:08 - COOLING_DOWN - refactor: Code cleanup in test_utils.py
 2024-11-02T11:52:46 - COOLING_DOWN - refactor: Code cleanup in test_utils.py
+2024-11-03T14:39:31 - COOLING_DOWN - docs: Add usage examples for profile
