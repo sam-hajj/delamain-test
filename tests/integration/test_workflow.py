8e0701959bf0006c078fab79cad1d0b9820c5e92
sam-hajj <sam@aceaicompany.com>
2024-11-12 11:08:40 +0000

    fix: Small bugfix in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -593,3 +593,4 @@
 2024-08-06T11:26:09 - MAINTENANCE_LOW - chore: Update dependencies (gradle)
 2024-08-06T12:33:10 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
 2024-11-12T11:03:05 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
+2024-11-12T11:08:40 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
