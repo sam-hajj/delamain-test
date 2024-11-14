fa0fe14b8bc36e4469374acf1940a48c18777976
sam-hajj <sam@aceaicompany.com>
2024-11-14 11:54:56 +0000

    chore: Update dependencies (yarn)

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -596,3 +596,4 @@
 2024-11-12T11:08:40 - MAINTENANCE_LOW - fix: Small bugfix in test_workflow.py
 2024-11-12T11:14:03 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
 2024-11-12T11:42:18 - MAINTENANCE_LOW - chore: Update dependencies (npm)
+2024-11-14T11:54:56 - MAINTENANCE_LOW - chore: Update dependencies (yarn)
