fcad16f1bcc16b758b61d722b61258aa57460fc7
sam-hajj <sam@aceaicompany.com>
2025-02-03 12:15:46 +0000

    ci: Tweak workflow triggers in test_workflow.py

--- tests/integration/test_workflow.py
+++ tests/integration/test_workflow.py
@@ -652,3 +652,4 @@
 2025-01-27T11:49:17 - MAINTENANCE_LOW - chore: Update dependencies (gradle)
 2025-02-01T14:17:57 - MAINTENANCE_LOW - docs: Minor clarification in test_workflow.py
 2025-02-01T14:35:20 - MAINTENANCE_LOW - style: Formatting adjustments in test_workflow.py
+2025-02-03T12:15:46 - MAINTENANCE_LOW - ci: Tweak workflow triggers in test_workflow.py
