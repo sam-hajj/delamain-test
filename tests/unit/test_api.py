e8766d4f79a806d5fdab9ce81efab0e9f4c16fd9
sam-hajj <sam@aceaicompany.com>
2025-02-27 15:21:20 +0000

    refactor: Modularize UI in test_api.py

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -270,3 +270,4 @@
 2025-02-24T10:55:33 - PROJECT_SHIFT - chore: Experiment with django in test_api.py
 2025-02-25T14:13:50 - PROJECT_SHIFT - refactor: Modularize database in test_api.py
 2025-02-26T12:54:39 - PROJECT_SHIFT - refactor: Adapt reporting for new requirements in test_api.py
+2025-02-27T15:21:20 - PROJECT_SHIFT - refactor: Modularize UI in test_api.py
