7b6bf246145186fe24a33ab6aafbace6ba7a189c
sam-hajj <sam@aceaicompany.com>
2025-02-27 15:00:55 +0000

    feat: POC for A/B testing framework in api

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -267,3 +267,4 @@
 2025-02-23T12:06:17 - PROJECT_SHIFT - refactor: Modularize backend in schemas.py
 2025-02-24T11:38:49 - PROJECT_SHIFT - chore: Experiment with pandas in schemas.py
 2025-02-26T12:00:11 - PROJECT_SHIFT - refactor: Modularize messaging queue in schemas.py
+2025-02-27T15:00:55 - PROJECT_SHIFT - feat: POC for A/B testing framework in api
