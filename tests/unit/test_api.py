5481723e5350aedd1ed3deca99b7517a66808281
sam-hajj <sam@aceaicompany.com>
2025-03-04 10:43:55 +0000

    refactor: Modularize messaging queue in test_api.py

--- tests/unit/test_api.py
+++ tests/unit/test_api.py
@@ -274,3 +274,4 @@
 2025-02-28T14:28:44 - PROJECT_SHIFT - refactor: Modularize database in test_api.py
 2025-02-28T14:41:25 - PROJECT_SHIFT - refactor: Adapt order for new requirements in test_api.py
 2025-03-04T10:14:28 - PROJECT_SHIFT - chore: Experiment with pandas in test_api.py
+2025-03-04T10:43:55 - PROJECT_SHIFT - refactor: Modularize messaging queue in test_api.py
