1cae85b1300959fae1ca85217de268ddd64dc84b
sam-hajj <sam@aceaicompany.com>
2025-04-10 10:35:36 +0000

    feat: Implement basic login in test_core.py

--- tests/unit/test_core.py
+++ tests/unit/test_core.py
@@ -173,3 +173,4 @@
 2024-02-05T13:57:41 - RAMPING_UP - feat: Implement basic caching in test_core.py
 2024-02-09T12:35:18 - RAMPING_UP - refactor: Improve inventory structure in unit
 2025-04-10T09:56:18 - RAMPING_UP - test: Add initial unit tests for test_core.py
+2025-04-10T10:35:36 - RAMPING_UP - feat: Implement basic login in test_core.py
